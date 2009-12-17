d57084972dca313d78910d3e0217ee11a5ac7880
Peter Nordin <peter.nordin@liu.se>
2009-12-17 15:48:24 +0000

    clean up ifdefs a bit

--- HOPSAN++/Utilities/TicToc.cc
+++ HOPSAN++/Utilities/TicToc.cc
@@ -3,8 +3,6 @@
 using namespace std;
 
 
-#ifdef WIN32
-
 TicToc::TicToc(const string prefix)
 {
     mPrefix = prefix;
@@ -13,50 +11,24 @@ TicToc::TicToc(const string prefix)
 
 void TicToc::Tic()
 {
+#ifdef WIN32
     mLastTime = GetTickCount();
+#else
+    clock_gettime(CLOCK_REALTIME, &mLastTime);
+#endif
 }
 
 double TicToc::Toc()
 {
+#ifdef WIN32
     int now_time;
     now_time = GetTickCount();
     return now_time - mLastTime;
-}
-
-double TicToc::TocPrint(const string prefix)
-{
-    double dt = Toc();
-    if (prefix.empty())
-    {
-        cout << mPrefix << ": " << dt << endl;
-    }
-    else
-    {
-        cout << prefix << ": " << dt << endl;
-    }
-
-    return dt;
-}
-
-
 #else
-
-TicToc::TicToc(const string prefix)
-{
-    mPrefix = prefix;
-    Tic();
-}
-
-void TicToc::Tic()
-{
-    clock_gettime(CLOCK_REALTIME, &mLastTime);
-}
-
-double TicToc::Toc()
-{
     timespec now_time;
     clock_gettime(CLOCK_REALTIME, &now_time);
     return CalcTimeDiff(now_time, mLastTime);
+#endif
 }
 
 double TicToc::TocPrint(const string prefix)
@@ -74,9 +46,9 @@ double TicToc::TocPrint(const string prefix)
     return dt;
 }
 
+#ifndef WIN32
 double TicToc::CalcTimeDiff(const timespec &time_now, const timespec &time_last)
 {
     return (double)(time_now.tv_sec - time_last.tv_sec) + ( (double)(time_now.tv_nsec - time_last.tv_nsec) )/1000000000.0;
 }
-
 #endif
--- HOPSAN++/Utilities/TicToc.h
+++ HOPSAN++/Utilities/TicToc.h
@@ -1,36 +1,24 @@
-#ifndef TIME_H_INCLUDED
-#define TIME_H_INCLUDED
+#ifndef TICTOC_H_INCLUDED
+#define TICTOC_H_INCLUDED
 
 #include <time.h>
 #include <string>
 
 class TicToc
 {
+public:
+    TicToc(const std::string prefix=std::string());
+    void Tic();
+    double Toc();
+    double TocPrint(const std::string prefix=std::string());
+private:
+    std::string mPrefix;
 #ifdef WIN32
-	public:
-		TicToc(const std::string prefix=std::string());
-		void Tic();
-		double Toc();
-		double TocPrint(const std::string prefix=std::string());
-	private:
-		std::string mPrefix;
-		int mLastTime;
-		double CalcTimeDiff(const int &time_now, const int &time_last);
-
+    int mLastTime;
 #else
-	public:
-		TicToc(const std::string prefix=std::string());
-		void Tic();
-		double Toc();
-		double TocPrint(const std::string prefix=std::string());
-
-	private:
-		std::string mPrefix;
-		timespec mLastTime;
-		double CalcTimeDiff(const timespec &time_now, const timespec &time_last);
+    timespec mLastTime;
+    double CalcTimeDiff(const timespec &time_now, const timespec &time_last);
 #endif
-
 };
 
-
-#endif // TIME_H_INCLUDED
+#endif // TICTOC_H_INCLUDED
