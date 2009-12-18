42fcba977dfb5d681274e07f192176629040faf6
Björn Eriksson <bjoer37@users.noreply.github.com>
2009-12-18 17:16:31 +0000

    fixed #elif MAC...

--- HOPSAN++/Utilities/TicToc.cc
+++ HOPSAN++/Utilities/TicToc.cc
@@ -17,6 +17,7 @@ void TicToc::Tic()
 {
 #ifdef WIN32
     mLastTime = GetTickCount();
+#elif defined MAC
 #else
     clock_gettime(CLOCK_REALTIME, &mLastTime);
 #endif
@@ -28,6 +29,8 @@ double TicToc::Toc()
     int now_time;
     now_time = GetTickCount();
     return (now_time - mLastTime)/1000.0; //Convert to seconds
+#elif defined MAC
+	return 0;
 #else
     timespec now_time;
     clock_gettime(CLOCK_REALTIME, &now_time);
@@ -51,8 +54,10 @@ double TicToc::TocPrint(const string prefix)
 }
 
 #ifndef WIN32
+#ifdef MAC
 double TicToc::CalcTimeDiff(const timespec &time_now, const timespec &time_last)
 {
     return (double)(time_now.tv_sec - time_last.tv_sec) + ( (double)(time_now.tv_nsec - time_last.tv_nsec) )/1000000000.0;
 }
+#endif
 #endif
