7b42872fa3fde1d351d40b2bcb30b67912a05325
Björn Eriksson <bjoer37@users.noreply.github.com>
2009-12-20 16:50:00 +0000

    fixed plot.py to be able to plot my results

--- HOPSAN++/plot.py
+++ HOPSAN++/plot.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 import pylab;
-pylab.plotfile("volumeC_P1.txt", (0,1,2), delimiter=' ');
+pylab.plotfile("volumeC_P1.txt", (0,1,2), checkrows=0, delimiter=' ');
 pylab.show();
