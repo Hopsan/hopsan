b55dd88d939cf86bc2c7f2a20154ce9a40af8cc8
Björn Eriksson <bjoer37@users.noreply.github.com>
2009-12-20 14:41:41 +0000

    implemented a TLM element and Q sources, seems to work

---
+++ HOPSAN++/Components/FlowSourceQ.hpp
@@ -0,0 +1,50 @@
+/*
+ *  FlowSourceQ.hpp
+ *  HOPSAN++
+ *
+ *  Created by Björn Eriksson on 2009-12-20.
+ *  Copyright 2009 LiU. All rights reserved.
+ *
+ */
+
+
+#ifndef FLOWSOURCEQ_HPP_INCLUDED
+#define FLOWSOURCEQ_HPP_INCLUDED
+
+class ComponentFlowSourceQ : public ComponentQ
+{
+	
+public:
+    enum {P1};
+	
+    ComponentFlowSourceQ(const string name, const double flow=1.0e-3, const double timestep=0.001)
+	:ComponentQ(name, timestep)
+    {
+        mFlow = flow;
+		
+        addPort(P1, Port("NodeHydraulic"));
+    }
+	
+    void simulateOneTimestep()
+    {
+        //read fron node
+   		Node* p1_ptr = mPorts[P1].getNodePtr();
+		
+		double c  = p1_ptr->getData(NodeHydraulic::WAVEVARIABLE);
+        double Zc = p1_ptr->getData(NodeHydraulic::CHARIMP);
+		
+        //delayed line
+        double q = mFlow;
+		double p = c + mFlow*Zc;
+		
+        //write to node
+        p1_ptr->setData(NodeHydraulic::MASSFLOW, q);
+        p1_ptr->setData(NodeHydraulic::PRESSURE, p);
+    }
+	
+private:
+    double mFlow;
+	
+};
+
+#endif // FLOWSOURCEQ_HPP_INCLUDED
---
+++ HOPSAN++/Components/PressureSourceQ.hpp
@@ -0,0 +1,50 @@
+/*
+ *  PressureSourceQ.hpp
+ *  HOPSAN++
+ *
+ *  Created by Björn Eriksson on 2009-12-20.
+ *  Copyright 2009 LiU. All rights reserved.
+ *
+ */
+
+
+#ifndef PRESSURESOURCEQ_HPP_INCLUDED
+#define PRESSURESOURCEQ_HPP_INCLUDED
+
+class ComponentPressureSourceQ : public ComponentQ
+{
+	
+public:
+    enum {P1};
+	
+    ComponentPressureSourceQ(const string name, const double pressure=1.0e5, const double timestep=0.001)
+	:ComponentQ(name, timestep)
+    {
+        mPressure = pressure;
+		
+        addPort(P1, Port("NodeHydraulic"));
+    }
+	
+    void simulateOneTimestep()
+    {
+        //read fron node
+   		Node* p1_ptr = mPorts[P1].getNodePtr();
+
+		double c  = p1_ptr->getData(NodeHydraulic::WAVEVARIABLE);
+        double Zc = p1_ptr->getData(NodeHydraulic::CHARIMP);
+				
+        //delayed line
+        double q = (mPressure - c)/Zc;
+		double p = mPressure;
+		
+        //write to node
+        p1_ptr->setData(NodeHydraulic::MASSFLOW, q);
+        p1_ptr->setData(NodeHydraulic::PRESSURE, p);
+    }
+	
+private:
+    double mPressure;
+	
+};
+
+#endif // PRESSURESOURCEQ_HPP_INCLUDED
