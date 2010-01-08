5b37f08c59396907c5e61202918f702e9b2796d2
Karl Pettersson <karl.pettersson@liu.se>
2010-01-08 13:27:25 +0000

    svn move

---
+++ HOPSAN++/HopsanCore/Components/Hydraulic/externalFlowSourceQ.hpp
@@ -0,0 +1,58 @@
+#ifndef EXTERNALFLOWSOURCEQ_HPP_INCLUDED
+#define EXTERNALFLOWSOURCEQ_HPP_INCLUDED
+
+#include <iostream>
+#include "HopsanCore.h"
+
+class ComponentExternalFlowSourceQ : public ComponentQ
+{
+private:
+    double mFlow;
+    enum {P1};
+
+public:
+    static Component *Creator()
+    {
+        std::cout << "running flowsourceQ creator" << std::endl;
+        return new ComponentExternalFlowSourceQ("DefaultFlowSourceQName");
+    }
+
+    ComponentExternalFlowSourceQ(const string name,
+                         const double flow     = 1.0e-3,
+                         const double timestep = 0.001)
+	: ComponentQ(name, timestep)
+    {
+        mFlow = flow;
+
+        addPort("P1", "NodeHydraulic", P1);
+
+        registerParameter("Flow", "Flöde", "m^3/s", mFlow);
+    }
+
+
+	void initialize()
+	{
+        //Nothing to initilize
+	}
+
+
+    void simulateOneTimestep()
+    {
+        //Get the nodes
+   		Node* p1_ptr = mPorts[P1].getNodePtr();
+
+        //Get variable values from nodes
+		double c  = p1_ptr->getData(NodeHydraulic::WAVEVARIABLE);
+        double Zc = p1_ptr->getData(NodeHydraulic::CHARIMP);
+
+        //Flow source equations
+        double q = mFlow;
+		double p = c + mFlow*Zc;
+
+        //Write new values to nodes
+        p1_ptr->setData(NodeHydraulic::MASSFLOW, q);
+        p1_ptr->setData(NodeHydraulic::PRESSURE, p);
+    }
+};
+
+#endif // EXTERNALFLOWSOURCEQ_HPP_INCLUDED
---
+++ HOPSAN++/HopsanCore/Components/Hydraulic/externalPressureSource.hpp
@@ -0,0 +1,64 @@
+#ifndef EXTERNALPRESSURESOURCE_HPP_INCLUDED
+#define EXTERNALPRESSURESOURCE_HPP_INCLUDED
+
+#include <iostream>
+#include "HopsanCore.h"
+
+class ComponentExternalPressureSource : public ComponentC
+{
+private:
+    double mStartPressure;
+    double mStartFlow;
+    double mZc;
+    double mPressure;
+    enum {P1};
+
+public:
+    static Component *Creator()
+    {
+        std::cout << "running pressuresource creator" << std::endl;
+        return new ComponentExternalPressureSource("DefaultPressureSourceName");
+    }
+
+    ComponentExternalPressureSource(const string name,
+                                    const double pressure       = 1.0e5,
+                                    const double timestep       = 0.001)
+        : ComponentC(name, timestep)
+    {
+        mStartPressure  = 0.0;
+        mStartFlow      = 0.0;
+        mPressure       = pressure;
+        mZc             = 0.0;
+
+        addPort("P1", "NodeHydraulic", P1);
+
+        registerParameter("P", "Pressure", "Pa", mPressure);
+    }
+
+
+    void initialize()
+    {
+        //read fron nodes
+   		Node* p1_ptr = mPorts[P1].getNodePtr();
+
+        //write to nodes
+        p1_ptr->setData(NodeHydraulic::PRESSURE, mStartPressure);
+        p1_ptr->setData(NodeHydraulic::MASSFLOW, mStartFlow);
+    }
+
+
+    void simulateOneTimestep()
+    {
+        //Get the nodes
+        Node* p1_ptr = mPorts[P1].getNodePtr();
+
+        //Orifice equations
+        double p = mPressure;
+
+        //Write new values to nodes
+        p1_ptr->setData(NodeHydraulic::WAVEVARIABLE, p);
+        p1_ptr->setData(NodeHydraulic::CHARIMP, mZc);
+    }
+};
+
+#endif // EXTERNALORIFICE_HPP_INCLUDED
