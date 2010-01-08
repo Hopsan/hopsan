e5ad7d919b40fd7da15f388818ce53f964dc402d
Karl Pettersson <karl.pettersson@liu.se>
2010-01-08 14:00:44 +0000

    Components.cc hydraulic

--- HOPSAN++/HopsanCore/Components/Components.cc
+++ HOPSAN++/HopsanCore/Components/Components.cc
@@ -6,14 +6,14 @@ DLLIMPORTEXPORT void register_components(ComponentFactory* cfact_ptr)
 {
     //Hydraulic components
     cfact_ptr->RegisterCreatorFunction();
-    cfact_ptr->RegisterCreatorFunction("ComponentExternalOrifice", ComponentExternalOrifice::Creator);
-    cfact_ptr->RegisterCreatorFunction("ComponentExternalVolume", ComponentExternalVolume::Creator);
-    cfact_ptr->RegisterCreatorFunction("ComponentExternalPressureSource", ComponentExternalPressureSource::Creator);
-    cfact_ptr->RegisterCreatorFunction("ComponentExternalFlowSourceQ", ComponentExternalFlowSourceQ::Creator);
-    cfact_ptr->RegisterCreatorFunction("ComponentExternalPressureSourceQ", ComponentExternalPressureSourceQ::Creator);
-    cfact_ptr->RegisterCreatorFunction("ComponentExternalFixedDisplacementPump", ComponentExternalFixedDisplacementPump::Creator);
-    cfact_ptr->RegisterCreatorFunction("ComponentExternalCheckValve", ComponentExternalCheckValve::Creator);
-    cfact_ptr->RegisterCreatorFunction("ComponentExternal43Valve", ComponentExternal43Valve::Creator);
+    cfact_ptr->RegisterCreatorFunction("HydraulicLaminarOrifice", ComponentExternalOrifice::Creator);
+    cfact_ptr->RegisterCreatorFunction("HydraulicVolume", ComponentExternalVolume::Creator);
+    cfact_ptr->RegisterCreatorFunction("HydraulicPressureSource", ComponentExternalPressureSource::Creator);
+    cfact_ptr->RegisterCreatorFunction("HydraulicFlowSourceQ", ComponentExternalFlowSourceQ::Creator);
+    cfact_ptr->RegisterCreatorFunction("HydraulicPressureSourceQ", ComponentExternalPressureSourceQ::Creator);
+    cfact_ptr->RegisterCreatorFunction("HydraulicFixedDisplacementPump", ComponentExternalFixedDisplacementPump::Creator);
+    cfact_ptr->RegisterCreatorFunction("HydraulicCheckValve", ComponentExternalCheckValve::Creator);
+    cfact_ptr->RegisterCreatorFunction("Hydraulic43Valve", ComponentExternal43Valve::Creator);
 
     //Signal components
     cfact_ptr->RegisterCreatorFunction("SignalSource", SignalSource::Creator);
