247d6eb738caf2a4250f70294b30b0b314cb0091
Peter Nordin <peterNordin@users.noreply.github.com>
2010-01-08 13:46:24 +0000

    Modified hopsanCore.h

--- HOPSAN++/HopsanCore/HopsanCore.h
+++ HOPSAN++/HopsanCore/HopsanCore.h
@@ -1,22 +1,9 @@
 #ifndef HOPSANCORE_H_INCLUDED
 #define HOPSANCORE_H_INCLUDED
 
-#include "Component.h"
-#include "Node.h"
-#include "Nodes/Nodes.h"
 #include "CoreUtilities/LoadExternal.h"
-
-#include "Components/PressureSource.hpp"
-#include "Components/Orifice.hpp"
-#include "Components/Volume.hpp"
-#include "Components/TLMlossless.hpp"
-#include "Components/TLMRlineR.hpp"
-#include "Components/PressureSourceQ.hpp"
-#include "Components/FlowSourceQ.hpp"
-#include "Components/Source.hpp"
-#include "Components/Sink.hpp"
-#include "Components/Gain.hpp"
-#include "Components/TurbOrifice.hpp"
+#include "Nodes/Nodes.h"
+#include "Components/Components.h"
 
 class HopsanEssentials
 {
