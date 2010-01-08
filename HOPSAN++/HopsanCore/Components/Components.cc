ca3993420bf8d395cd1acecf70208ec12580f62a
Karl Pettersson <karl.pettersson@liu.se>
2010-01-08 13:34:29 +0000

    components.cc

--- HOPSAN++/HopsanCore/Components/Components.cc
+++ HOPSAN++/HopsanCore/Components/Components.cc
@@ -6,7 +6,14 @@ DLLIMPORTEXPORT void register_components(ComponentFactory* cfact_ptr)
 {
     //Hydraulic components
     cfact_ptr->RegisterCreatorFunction();
-
+    cfact_ptr->RegisterCreatorFunction("ComponentExternalOrifice", ComponentExternalOrifice::Creator);
+    cfact_ptr->RegisterCreatorFunction("ComponentExternalVolume", ComponentExternalVolume::Creator);
+    cfact_ptr->RegisterCreatorFunction("ComponentExternalPressureSource", ComponentExternalPressureSource::Creator);
+    cfact_ptr->RegisterCreatorFunction("ComponentExternalFlowSourceQ", ComponentExternalFlowSourceQ::Creator);
+    cfact_ptr->RegisterCreatorFunction("ComponentExternalPressureSourceQ", ComponentExternalPressureSourceQ::Creator);
+    cfact_ptr->RegisterCreatorFunction("ComponentExternalFixedDisplacementPump", ComponentExternalFixedDisplacementPump::Creator);
+    cfact_ptr->RegisterCreatorFunction("ComponentExternalCheckValve", ComponentExternalCheckValve::Creator);
+    cfact_ptr->RegisterCreatorFunction("ComponentExternal43Valve", ComponentExternal43Valve::Creator);
 
     //Signal components
 
