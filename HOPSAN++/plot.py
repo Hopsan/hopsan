b0cc1b391cfc6a818dc140dc4fe9025d8b87fd89
Björn Eriksson <bjoer37@users.noreply.github.com>
2010-01-11 07:57:46 +0000

    Oops, plot.py changed...

--- HOPSAN++/plot.py
+++ HOPSAN++/plot.py
@@ -7,6 +7,8 @@ if len(sys.argv) < 2:
 else:
   filename = sys.argv[1]
 print "Plotting " + filename
-#pylab.plotfile(filename, (0,1,2), checkrows=0, delimiter=' ', names=['Time', "Flow", "Pressure"]);
-pylab.plotfile(filename, (0,1), checkrows=0, delimiter=' ', names=['Time', "Value"]);
+# Used for plotting Hydraulic nodes:
+pylab.plotfile(filename, (0,1,2), checkrows=0, delimiter=' ', names=['Time', "Flow", "Pressure"]);
+# Used for plotting Signal nodes:
+#pylab.plotfile(filename, (0,1), checkrows=0, delimiter=' ', names=['Time', "Value"]);
 pylab.show();
