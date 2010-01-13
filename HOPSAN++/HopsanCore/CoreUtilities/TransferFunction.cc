ee5ef62e0663df6ed5f96db5c7a40a234f8202ee
Karl Pettersson <karl.pettersson@liu.se>
2010-01-13 13:20:52 +0000

    TransferFunction

--- HOPSAN++/HopsanCore/CoreUtilities/TransferFunction.cc
+++ HOPSAN++/HopsanCore/CoreUtilities/TransferFunction.cc
@@ -6,27 +6,32 @@ TransferFunction::TransferFunction()
 {
 }
 
-void TransferFunction::Initialize()
+TransferFunction::TransferFunction(double num [3], double den [3], double timestep)
 {
-
+    for(int i=0; i<3; i++)
+    {
+      mnum[i] = num[i];
+      mden[i] = den[i];
+    }
+    mTimestep = timestep;
 }
 
-double TransferFunction::Filter(double signal, const double num [3], const double den [3], double timestep )
+double TransferFunction::filter(double signal)
 {
-        u[2] = u[1];
-        u[1] = u[0];
-        u[0] = signal;
+    u[2] = u[1];
+    u[1] = u[0];
+    u[0] = signal;
 
-        y[2] = y[1];
-        y[1] = y[0];
+    y[2] = y[1];
+    y[1] = y[0];
 
-        b[0] = 4.0*num[2]+num[0]*pow(timestep,2.0)+2.0*num[1]*timestep;
-        b[1] = -8.0*num[2]+2*num[0]*pow(timestep,2.0);
-        b[2] = -2.0*num[1]*timestep+4.0*num[2]+num[0]*pow(timestep,2.0);
+    b[0] = 4.0*mnum[2]+mnum[0]*pow(mTimestep,2.0)+2.0*mnum[1]*mTimestep;
+    b[1] = -8.0*mnum[2]+2*mnum[0]*pow(mTimestep,2.0);
+    b[2] = -2.0*mnum[1]*mTimestep+4.0*mnum[2]+mnum[0]*pow(mTimestep,2.0);
 
-        a[0] = 4.0*den[2]+den[0]*pow(timestep,2.0)+2.0*den[1]*timestep;
-        a[1] = -8.0*den[2]+2.0*den[0]*pow(timestep,2.0);
-        a[2] = -2.0*den[1]*timestep+4.0*den[2]+den[0]*pow(timestep,2.0);
+    a[0] = 4.0*mden[2]+mden[0]*pow(mTimestep,2.0)+2.0*mden[1]*mTimestep;
+    a[1] = -8.0*mden[2]+2.0*mden[0]*pow(mTimestep,2.0);
+    a[2] = -2.0*mden[1]*mTimestep+4.0*mden[2]+mden[0]*pow(mTimestep,2.0);
 
         /* Equation:
                 bo+b1q^-1+b2q^-2
@@ -38,3 +43,12 @@ double TransferFunction::Filter(double signal, const double num [3], const doubl
         return y[0];
 }
 
+void TransferFunction::setCoefficients(double num [3], double den [3], double timestep)
+{
+    for(int i=0; i<3; i++)
+    {
+      mnum[i] = num[i];
+      mden[i] = den[i];
+    }
+    mTimestep = timestep;
+}
--- HOPSAN++/HopsanCore/CoreUtilities/TransferFunction.h
+++ HOPSAN++/HopsanCore/CoreUtilities/TransferFunction.h
@@ -8,13 +8,17 @@ class DLLIMPORTEXPORT TransferFunction
 {
 public:
     TransferFunction();
-    void Initialize();
-    double Filter(double signal, const double num [3], const double den [3], double timestep);
+    TransferFunction(double num [3], double den [3], double timestep);
+    void setCoefficients(double num [3], double den [3], double timestep);
+    double filter(double signal);
 private:
     double u [3];
 	double y [3];
 	double a [3];
 	double b [3];
+	double mnum [3];
+	double mden [3];
+	double mTimestep;
 };
 
 #endif // TRANSFERFUNCTION_H_INCLUDED
