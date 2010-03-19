1bd0e2d4bcc864ffa5dc1709211ba9d1bbd8ac3f
Karl Pettersson <karl.pettersson@liu.se>
2010-03-19 14:32:02 +0000

    HydraulicTurbulentOrifice.hpp

--- HOPSAN++/HopsanCore/Components/Hydraulic/HydraulicTurbulentOrifice.hpp
+++ HOPSAN++/HopsanCore/Components/Hydraulic/HydraulicTurbulentOrifice.hpp
@@ -1,3 +1,11 @@
+//!
+//! @file   HydraulicTurbulentOrifice.hpp
+//! @author Karl Pettersson
+//! @date   2009-12-21
+//!
+//! @brief Contains a Hydraulic Turbulent Orifice of Q-type
+//! @ingroup HydraulicComponents
+//!
 #ifndef HYDRAULICTURBULENTORIFICE_HPP_INCLUDED
 #define HYDRAULICTURBULENTORIFICE_HPP_INCLUDED
 
@@ -5,10 +13,6 @@
 #include "../../ComponentUtilities.h"
 #include "math.h"
 
-//!
-//! @brief A hydraulic turbulent orifice
-//! @ingroup HydraulicComponents
-//!
 class HydraulicTurbulentOrifice : public ComponentQ
 {
 private:
@@ -16,21 +20,11 @@ private:
     double mA;
     double mKc;
     TurbulentFlowFunction qTurb;
-    enum {P1, P2};
-//    double sign(double x)
-//    {
-//        return x/fabs(x);
-//    }
-//    double sigsqrl(double x,
-//            const double x0=0.001,
-//            const double r=4)
-//    {
-//        return sign(x)*pow(pow(fabs(x),r)/(pow(fabs(x),(r/2))+pow(x0,r)),(1/r));
-//    }
+    Port *mpP1, *mpP2;
+
 public:
     static Component *Creator()
     {
-        std::cout << "running orifice creator" << std::endl;
         return new HydraulicLaminarOrifice("TurbulentOrifice");
     }
 
@@ -45,9 +39,8 @@ public:
         mA = A;
         mKc = mCq*mA*sqrt(2.0/890.0);
 
-        addPowerPort("P1", "NodeHydraulic", P1);
-        addPowerPort("P2", "NodeHydraulic", P2);
-        //addMultiPort("P", "NodeHydraulic", 2);
+        mpP1 = addPowerPort("P1", "NodeHydraulic");
+        mpP2 = addPowerPort("P2", "NodeHydraulic");
 
         registerParameter("Cq", "Flow coefficient", "[-]", mCq);
         registerParameter("A", "Area", "m^3", mA);
@@ -62,10 +55,10 @@ public:
     void simulateOneTimestep()
     {
         //Get variable values from nodes
-        double c1 = mPortPtrs[P1]->readNode(NodeHydraulic::WAVEVARIABLE);
-        double Zc1 = mPortPtrs[P1]->readNode(NodeHydraulic::CHARIMP);
-        double c2 = mPortPtrs[P2]->readNode(NodeHydraulic::WAVEVARIABLE);
-        double Zc2 = mPortPtrs[P2]->readNode(NodeHydraulic::CHARIMP);
+        double c1 = mpP1->readNode(NodeHydraulic::WAVEVARIABLE);
+        double Zc1 = mpP1->readNode(NodeHydraulic::CHARIMP);
+        double c2 = mpP2->readNode(NodeHydraulic::WAVEVARIABLE);
+        double Zc2 = mpP2->readNode(NodeHydraulic::CHARIMP);
 
         //Orifice equations
         double q2 = qTurb.getFlow(c1,c2,Zc1,Zc2);
@@ -74,10 +67,10 @@ public:
         double p2 = c2 + q2*Zc2;
 
         //Write new values to nodes
-        mPortPtrs[P1]->writeNode(NodeHydraulic::PRESSURE, p1);
-        mPortPtrs[P1]->writeNode(NodeHydraulic::MASSFLOW, q1);
-        mPortPtrs[P2]->writeNode(NodeHydraulic::PRESSURE, p2);
-        mPortPtrs[P2]->writeNode(NodeHydraulic::MASSFLOW, q2);
+        mpP1->writeNode(NodeHydraulic::PRESSURE, p1);
+        mpP1->writeNode(NodeHydraulic::MASSFLOW, q1);
+        mpP2->writeNode(NodeHydraulic::PRESSURE, p2);
+        mpP2->writeNode(NodeHydraulic::MASSFLOW, q2);
     }
 };
 
