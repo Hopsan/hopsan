fe094fa4a16a58a8dd7fbf585e5786d6c7f611dc
Peter Nordin <peterNordin@users.noreply.github.com>
2010-05-24 08:58:37 +0000

    Added OpenModelica license to all gui files (also containing our names), make sure you are using UTF8 to avoid messing upp all åäö in the comments

--- HOPSAN++/HopsanGUI/OptionsWidget.h
+++ HOPSAN++/HopsanGUI/OptionsWidget.h
@@ -1,3 +1,41 @@
+/*
+ * This file is part of OpenModelica.
+ *
+ * Copyright (c) 1998-CurrentYear, Linköping University,
+ * Department of Computer and Information Science,
+ * SE-58183 Linköping, Sweden.
+ *
+ * All rights reserved.
+ *
+ * THIS PROGRAM IS PROVIDED UNDER THE TERMS OF GPL VERSION 3 
+ * AND THIS OSMC PUBLIC LICENSE (OSMC-PL). 
+ * ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS PROGRAM CONSTITUTES RECIPIENT'S  
+ * ACCEPTANCE OF THE OSMC PUBLIC LICENSE.
+ *
+ * The OpenModelica software and the Open Source Modelica
+ * Consortium (OSMC) Public License (OSMC-PL) are obtained
+ * from Linköping University, either from the above address,
+ * from the URLs: http://www.ida.liu.se/projects/OpenModelica or  
+ * http://www.openmodelica.org, and in the OpenModelica distribution. 
+ * GNU version 3 is obtained from: http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without
+ * even the implied warranty of  MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE, EXCEPT AS EXPRESSLY SET FORTH
+ * IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE CONDITIONS
+ * OF OSMC-PL.
+ *
+ * See the full OSMC Public License conditions for more details.
+ *
+ */
+
+/*
+ * Main Authors 2009-2010:  Robert Braun, Björn Eriksson, Peter Nordin
+ * Contributors 2009-2010:  Mikael Axin, Alessandro Dell'Amico, Karl Pettersson, Ingo Staack
+ * Fluid and Mechatronic Systems, Department of Management and Engineering, Linköping University
+ * Based on: HopsanGUI
+ */
+
 //$Id: OptionsWidget.h 1195 2010-04-01 09:25:58Z robbr48 $
 
 #ifndef OptionsWidget_H
