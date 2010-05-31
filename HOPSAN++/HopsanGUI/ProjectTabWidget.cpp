a29860611f8f8790793369fd5c50868139016e91
Karl Pettersson <karl.pettersson@liu.se>
2010-05-31 14:35:51 +0000

    Fixed bug with dissapearing ports after removing connector lines

--- HOPSAN++/HopsanGUI/ProjectTabWidget.cpp
+++ HOPSAN++/HopsanGUI/ProjectTabWidget.cpp
@@ -572,15 +572,20 @@ void GraphicsView::mousePressEvent(QMouseEvent *event)
     emit viewClicked();
     mJustStoppedCreatingConnector = false;
 
-    this->setDragMode(RubberBandDrag);
     if (this->mIsCreatingConnector)
     {
         this->setDragMode(NoDrag);
     }
+    else
+    {
+        this->setDragMode(RubberBandDrag);
+    }
+
     if (event->button() == Qt::RightButton and this->mIsCreatingConnector)
     {
         if((mpTempConnector->getNumberOfLines() == 1 and mpTempConnector->isMakingDiagonal()) or (mpTempConnector->getNumberOfLines() == 2 and !mpTempConnector->isMakingDiagonal()))
         {
+            mpTempConnector->getStartPort()->show();
             mIsCreatingConnector = false;
             mJustStoppedCreatingConnector = true;
         }
