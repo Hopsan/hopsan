82348df2d63009b695a0434aa80b954f4ed29c5a
Peter Nordin <peterNordin@users.noreply.github.com>
2010-09-24 12:22:07 +0000

    removed commeted guisystemobject

--- HOPSAN++/HopsanGUI/GUIObject.h
+++ HOPSAN++/HopsanGUI/GUIObject.h
@@ -264,39 +264,6 @@ private:
 };
 
 
-//class GUISubsystem : public GUIContainerObject
-//{
-//    Q_OBJECT
-//public:
-//    GUISubsystem(AppearanceData appearanceData, QPoint position, qreal rotation, GUISystem *system, selectionStatus startSelected = DESELECTED, graphicsType gfxType = USERGRAPHICS, QGraphicsItem *parent = 0);
-//
-//    void deleteInHopsanCore();
-//
-//    QString getTypeName();
-//    void setName(QString newName, renameRestrictions renameSettings=UNRESTRICTED);
-//    void setTypeCQS(QString typestring);
-//    QString getTypeCQS();
-//    void loadFromFile(QString modelFileName=QString());
-//
-//    void saveToTextStream(QTextStream &rStream, QString prepend);
-//
-//    QVector<QString> getParameterNames();
-//
-//    enum { Type = UserType + 4 };
-//    int type() const;
-//
-//protected:
-//    void contextMenuEvent(QGraphicsSceneContextMenuEvent *event);
-//    void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event);
-//    void openParameterDialog();
-//    void createPorts();
-//
-//private:
-//    QString mModelFilePath;
-//    //QString mGraphicsFilePath;
-//    bool   mIsEmbedded;
-//    QString mLoadType;
-//};
 
 
 class GUISystemPort : public GUIObject
