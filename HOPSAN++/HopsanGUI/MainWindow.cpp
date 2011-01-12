081b8d2d6b32fab91c2441ce6509ad37a2ff8343
Robert Braun <robert.braun@liu.se>
2011-01-12 13:40:03 +0000

    Re-added dummy components in a new Benchmarking library group

--- HOPSAN++/HopsanGUI/MainWindow.cpp
+++ HOPSAN++/HopsanGUI/MainWindow.cpp
@@ -123,6 +123,8 @@ MainWindow::MainWindow(QWidget *parent)
         mpLibrary->addExternalLibrary(gConfig.getUserLibs().at(i));
     }
 
+    mpLibrary->addLibrary(QString(COMPONENTPATH) + "benchmarking");
+
     mpLibrary->addLibrary(QString(COMPONENTPATH) + "Subsystem");
 
     mpLibrary->addEmptyLibrary("Signal");
@@ -132,7 +134,6 @@ MainWindow::MainWindow(QWidget *parent)
     mpLibrary->addLibrary(QString(COMPONENTPATH) + "signal/Filters","Signal");
     mpLibrary->addLibrary(QString(COMPONENTPATH) + "signal/Logic","Signal");
     mpLibrary->addLibrary(QString(COMPONENTPATH) + "signal/Simulation Control","Signal");
-    //mpLibrary->addLibrary(QString(COMPONENTPATH) + "signal/Dummy","Signal");
 
     mpLibrary->addEmptyLibrary("Mechanic");
     mpLibrary->addLibrary(QString(COMPONENTPATH) + "mechanic/Transformers","Mechanic");
@@ -148,9 +149,6 @@ MainWindow::MainWindow(QWidget *parent)
     mpLibrary->addLibrary(QString(COMPONENTPATH) + "hydraulic/actuators","Hydraulic");
     mpLibrary->addLibrary(QString(COMPONENTPATH) + "hydraulic/valves","Hydraulic");
     mpLibrary->addLibrary(QString(COMPONENTPATH) + "hydraulic/pumps","Hydraulic");
-    //mpLibrary->addLibrary(QString(COMPONENTPATH) + "hydraulic/Dummy","Hydraulic");
-
-    //mpLibrary->addLibrary(QString(COMPONENTPATH) + "_Optimized");
 
     //Create one new project tab, IMPORTANT: must be after Subsystem library has been loaded as we need Subsystem Appearance
     mpProjectTabs->addNewProjectTab();
