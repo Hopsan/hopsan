73fba634296507cd982f6045b850446e56515f42
Peter Nordin <peterNordin@users.noreply.github.com>
2011-05-16 12:02:49 +0000

    Added rather restrictive initial license header to all cc cpp hpp and h files

--- HOPSAN++/HopsanCore/ComponentUtilities/Integrator.h
+++ HOPSAN++/HopsanCore/ComponentUtilities/Integrator.h
@@ -1,3 +1,18 @@
+/*-----------------------------------------------------------------------------
+ This source file is part of Hopsan NG
+
+ Copyright (c) 2011 
+    Mikael Axin, Robert Braun, Alessandro Dell'Amico, Björn Eriksson,
+    Peter Nordin, Karl Pettersson, Petter Krus, Ingo Staack
+
+ This file is provided "as is", with no guarantee or warranty for the
+ functionality or reliability of the contents. All contents in this file is
+ the original work of the copyright holders at the Division of Fluid and
+ Mechatronic Systems (Flumes) at Linköping University. Modifying, using or
+ redistributing any part of this file is prohibited without explicit
+ permission from the copyright holders.
+-----------------------------------------------------------------------------*/
+
 //!
 //! @file   Integrator.h
 //! @author Björn Eriksson <bjorn.eriksson@liu.se>
