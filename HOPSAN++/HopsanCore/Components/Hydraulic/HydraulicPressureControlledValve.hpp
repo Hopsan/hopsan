8df94c4b8af7db985ec3a625ed0feb44132c8c17
Björn Eriksson <bjoer37@users.noreply.github.com>
2011-08-10 14:53:33 +0000

    Added pnom as a parameter.

--- HOPSAN++/HopsanCore/Components/Hydraulic/HydraulicPressureControlledValve.hpp
+++ HOPSAN++/HopsanCore/Components/Hydraulic/HydraulicPressureControlledValve.hpp
@@ -75,7 +75,8 @@ namespace hopsan {
             registerParameter("tao", "Time Constant of Spool", "[s]", tao);
             registerParameter("k_c,s", "Steady State Characteristic due to Spring", "[(m^3/s)/Pa]", Kcs);
             registerParameter("k_c,f", "Steady State Characteristic due to Flow Forces", "[(m^3/s)/Pa]", Kcf);
-            registerParameter("q_nom", "Flow with Fully Open Valve and pressure drop Pnom", "[m^3/s]", qnom);
+            registerParameter("q_nom", "Flow with Fully Open Valve and pressure drop p_nom", "[m^3/s]", qnom);
+            registerParameter("p_nom", "Nominal pressure drop", "[Pa]", pnom);
             registerParameter("p_h", "Hysteresis Width", "[Pa]", ph);
         }
 
