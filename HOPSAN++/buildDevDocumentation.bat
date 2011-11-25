b3dab023f7ba263618ccffde71b178b59581ff23
Peter Nordin <peterNordin@users.noreply.github.com>
2011-11-25 15:48:34 +0000

    Split the documentation into two different folder, using one common grahics folder for common graphics. Two different .bat files, for generating either pure user documentation or FULL developer documentation.

---
+++ HOPSAN++/buildDevDocumentation.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+cd docs
+doxygen devDoxyfile
+echo.
+echo You can read the documentation by opening the file ./docs/dev/html/index.html
+echo.
+pause
---
+++ HOPSAN++/buildUserDocumentation.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+cd docs
+doxygen userDoxyfile
+echo.
+echo You can read the documentation by opening the file ./docs/user/html/index.html
+echo.
