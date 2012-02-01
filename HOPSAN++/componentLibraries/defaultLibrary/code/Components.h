5225aaa22aa4a6f4899d2b82f5d657a2ff8aa85a
Petter Krus <petkr14@users.noreply.github.com>
2012-02-01 07:35:03 +0000

    Electric sources and capacitace multi port added

--- HOPSAN++/componentLibraries/defaultLibrary/code/Components.h
+++ HOPSAN++/componentLibraries/defaultLibrary/code/Components.h
@@ -180,15 +180,18 @@
 /* Electric Components */
 #include "Electric/ElectricBattery.hpp"
 #include "Electric/ElectricCapacitance2.hpp"
+#include "Electric/ElectricCapacitanceMultiPort.hpp"
 #include "Electric/ElectricGround.hpp"
 #include "Electric/ElectricMotor.hpp"
 #include "Electric/ElectricMotorGear.hpp"
 #include "Electric/ElectricIcontroller.hpp"
 #include "Electric/ElectricInductance.hpp"
 #include "Electric/ElectricIsource.hpp"
+#include "Electric/ElectricIsensor.hpp"
 #include "Electric/ElectricResistor.hpp"
 #include "Electric/ElectricVarResistor.hpp"
 #include "Electric/ElectricSwitch.hpp"
 #include "Electric/ElectricUsource.hpp"
+#include "Electric/ElectricUsensor.hpp"
 
 #endif // COMPONENTS_H_INCLUDED
