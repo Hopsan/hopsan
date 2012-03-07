15782963aa93c74252fc59b6affadb3a946f700b
Peter Nordin <peterNordin@users.noreply.github.com>
2012-03-07 07:06:54 +0000

    More work on debBuild scripts. Also fixed incorect copy option on linux systems, now we no longer dereference symlinks

--- HOPSAN++/buildDebPackage/makeDebRelease.sh
+++ HOPSAN++/buildDebPackage/makeDebRelease.sh
@@ -5,5 +5,8 @@ version=0.5.3
 
 svn export hopsan-0.0.0 hopsan-$version
 cd hopsan-$version
+rm debian/changelog
+dch -p -M --create --package hopsan --newversion=$version See Hopsan-release-notes.txt for changes
+dch -p -m --release ""
 debuild -us -uc --lintian-opts --color always -X files
 cd ..
