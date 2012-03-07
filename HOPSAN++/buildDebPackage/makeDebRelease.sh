8163b5ca215cf836bf4a11625b3c36f19eb8ecf8
Peter Nordin <peterNordin@users.noreply.github.com>
2012-03-07 21:07:50 +0000

    more work on deb scripts

--- HOPSAN++/buildDebPackage/makeDebRelease.sh
+++ HOPSAN++/buildDebPackage/makeDebRelease.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 # $Id$
 
+name=hopsan
 version=0.5.3
 
-svn export hopsan-0.0.0 hopsan-$version
-cd hopsan-$version
+# TODO: should ask user for version input maybe
+
+# TODO: shoudl run sed on version files, splash screen and such
+
+
+# Determine deb dir name
+packagedir=$name-$version
+# First clear dir if it already exist
+rm -r $packagedir
+# Export template
+svn export hopsan-0.0.0 $packagedir
+cd $packagedir
+# Generate NEW changelog file for this release version with no content in particular
 rm debian/changelog
-dch -p -M --create --package hopsan --newversion=$version See Hopsan-release-notes.txt for changes
+dch -p -M --create --package $name --newversion=$version See Hopsan-release-notes.txt for changes
 dch -p -m --release ""
+# Now lets create and test the package
 debuild -us -uc --lintian-opts --color always -X files
 cd ..
+
+# TODO: Should try to put output in some special folder
+
+# TODO: MAYBE cleanup build dir, or ask user, or not
