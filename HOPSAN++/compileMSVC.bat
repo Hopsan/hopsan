7a094fd2385d7d5257c4192e590fa08833b78100
Peter Nordin <peterNordin@users.noreply.github.com>
2012-07-05 11:10:15 +0000

    Reverse Merged revision(s) 4554 from trunk/HOPSAN++: Removed two obsolete files ........
    Turns out that they were not obsolete at all, they are needed by python build script

---
+++ HOPSAN++/compileMSVC.bat
@@ -0,0 +1,20 @@
+:: 1 = version
+:: 2 = architecture
+:: 3 = msvcDir
+:: 4 = qmakeDir
+:: 5 = buildDir
+:: 6 = jomDir
+:: 7 = hopsanDir
+
+setlocal enabledelayedexpansion
+
+echo off
+
+call %3\SetEnv.cmd /Release /%2
+COLOR 07
+call %4\qtenv2.bat
+cd %5
+call %6\jom.exe clean
+call %4\qmake.exe %7\HopsanCore\HopsanCore.pro -r -spec win32-msvc%1 "CONFIG+=release" "QMAKE_CXXFLAGS_RELEASE += -wd4251"
+call %6\jom.exe
+cd ..
