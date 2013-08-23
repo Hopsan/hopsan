6ec2f145c4ab3889ead2b71807b690952bb135e2
Petter Krus <petkr14@users.noreply.github.com>
2013-08-23 08:59:45 +0000

    signedSquareL is modified

--- HOPSAN++/HopsanCore/src/ComponentUtilities/AuxiliarySimulationFunctions.cc
+++ HOPSAN++/HopsanCore/src/ComponentUtilities/AuxiliarySimulationFunctions.cc
@@ -95,14 +95,17 @@ bool hopsan::fuzzyEqual(const double x, const double y, const double epsilon)
 //! @ingroup AuxiliarySimulationFunctions
 double hopsan::signedSquareL(const double x, const double x0)
 {
-    return (-sqrt(x0) + sqrt(x0 + fabs(x))) * sign(x);
+//    return (-sqrt(x0) + sqrt(x0 + fabs(x))) * sign(x);
+    return fabs(x)*pow(1/(pow(x0,4) + pow(fabs(x),2)),0.25)*sign(x);
 }
 
 
 //! @ingroup AuxiliarySimulationFunctions
 double hopsan::dxSignedSquareL(const double x, const double x0)
 {
-    return (1.0 / (sqrt(x0 + fabs(x)) * 2.0));
+//    return (1.0 / (sqrt(x0 + fabs(x)) * 2.0));
+    return (pow(1/(pow(x0,4) + pow(fabs(x),2)),1.25)*(2*pow(x0,4) + pow(fabs(x),2))*
+            dxAbs(x)*sign(x))/2.;
 }
 
 
