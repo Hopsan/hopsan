c0f5af5eea647f05393ff36c6429564bfba67b57
Robert Braun <robert.braun@liu.se>
2014-03-29 14:50:26 +0000

    Added a splash screen.

--- HoLC/main.cpp
+++ HoLC/main.cpp
@@ -1,9 +1,21 @@
-#include "MainWindow.h"
 #include <QApplication>
+#include <QPixmap>
+#include <QSplashScreen>
+#include <QTimer>
+
+#include "MainWindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QPixmap pixmap(":/gfx/graphics/splash.png");
+    QSplashScreen *pSplash = new QSplashScreen(pixmap, Qt::WindowStaysOnTopHint);
+    pSplash->showMessage("Starting HoLC...");
+    pSplash->show();
+    pSplash->setAttribute(Qt::WA_DeleteOnClose, true);
+    QTimer::singleShot(2000, pSplash, SLOT(close()));
+
     MainWindow w;
     w.showMaximized();
 
