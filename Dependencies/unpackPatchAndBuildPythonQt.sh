d0c9007d7f5b2fe9ef0e84a0d865282dadfe349f
Peter Nordin <peterNordin@users.noreply.github.com>
2015-02-17 14:13:55 +0000

    Updated to PythonQt3.0 on linux

--- Dependencies/unpackPatchAndBuildPythonQt.sh
+++ Dependencies/unpackPatchAndBuildPythonQt.sh
@@ -21,50 +21,31 @@ if [ "$2" != "" ]; then
   pyversion="$2"  
 fi
 
-ubuntuversion=$(echo `lsb_release -rs` | sed 's|\.||')
+#ubuntuversion=$(echo `lsb_release -rs` | sed 's|\.||')
 
-if [ $ubuntuversion -lt "1204" ]; then
+pythonqtname="PythonQt3.0"
 
-  pythonqtname="PythonQt2.0.1"
-
-  # Abort if dir already exist. When running release build script we dont want to build twice
-  if [ -d $pythonqtname ]; then
-    echo "Directory $pythonqtname already exist. Remove it if you want (re)build using this script."
-    exit 0
-  fi
-
-  rm -rf $pythonqtname
-  unzip -q $pythonqtname.zip
-    
-  echo "Applying Hopsan fixes to code"
-  # Apply patch to remove qt all extensions and cocoa thing
-  patch -p0 < PythonQt2.0.1_ubuntu.patch
-
-else
-
-  pythonqtname="PythonQt2.1_Qt4.8"
-
-  # Abort if dir already exist. When running release build script we dont want to build twice
-  if [ -d $pythonqtname ]; then
-    echo "Directory $pythonqtname already exist. Remove it if you want (re)build using this script."
-    exit 0
-  fi
-
-  rm -rf $pythonqtname
-  unzip -q $pythonqtname.zip
-
-  cd $pythonqtname
-  echo "Applying Hopsan fixes to code"
-  
-  # Apply patch to remove qt all extensions and other things
-  if [ -f ../$pythonqtname\_ubuntu.patch ]; then
-    patch -p0 < ../$pythonqtname\_ubuntu.patch
-  fi
-
-  # Remove tests and examples, allways (test would not build at all for r209)
-  #sed "s|tests examples||" -i PythonQt.pro
-  cd ..
+# Abort if dir already exist. When running release build script we dont want to build twice
+if [ -d $pythonqtname ]; then
+echo "Directory $pythonqtname already exist. Remove it if you want (re)build using this script."
+exit 0
 fi
+
+rm -rf $pythonqtname
+unzip -q $pythonqtname.zip
+
+cd $pythonqtname
+echo "Applying Hopsan fixes to code"
+
+# Apply patch to remove qt all extensions and other things
+if [ -f ../$pythonqtname\_ubuntu.patch ]; then
+patch -p0 < ../$pythonqtname\_ubuntu.patch
+fi
+
+# Remove tests and examples, always (test would not build at all for r209)
+#sed "s|tests examples||" -i PythonQt.pro
+cd ..
+
 
 cd $pythonqtname
 
