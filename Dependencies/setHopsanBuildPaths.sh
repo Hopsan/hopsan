be2d76e36f1bda21670743f5bb4439177e16eb57
Magnus Sethson <magnus@sethson.se>
2015-02-22 11:50:56 +0000

    By using the macports package qt5-mac we get a similar system as linux on mac. The purpose of this file may be questioned.

--- Dependencies/setHopsanBuildPaths.sh
+++ Dependencies/setHopsanBuildPaths.sh
@@ -5,13 +5,16 @@
 # Author: Magnus Sethson magnus.sethson@liu.se
 # Date:   2015-02-20
 
-if [ "$OSTYPE" == "darwin14" ]; then
-    export hopsan_qt_path=$HOME/Qt/5.4
-    export hopsan_qt_bin=$hopsan_qt_path/bin/
-    export hopsan_qt_qmake=$hopsan_qt_bin/qmake
-else
-    export hopsan_qt_path=
-    export hopsan_qt_bin=
-    export hopsan_qt_qmake=qmake
-fi
+# By using Qt from the macports qt5-mac package there is no difference between mac and linux
 
+#if [ "$OSTYPE" == "darwin14" ]; then
+#    export hopsan_qt_path=$HOME/Qt/5.4
+#    export hopsan_qt_bin=$hopsan_qt_path/bin/
+#    export hopsan_qt_qmake=$hopsan_qt_bin/qmake
+#else
+#    export hopsan_qt_path=
+#    export hopsan_qt_bin=
+#    export hopsan_qt_qmake=qmake
+#fi
+
+export hopsan_qt_qmake=qmake
