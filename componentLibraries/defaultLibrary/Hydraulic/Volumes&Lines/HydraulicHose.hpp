0a39c1d3d172efd38b9310fa693630dfc2215c21
Peter Nordin <peterNordin@users.noreply.github.com>
2015-03-05 13:20:25 +0000

    Rewrote hardcoded circle buffer into initialize allocated version for "dynamic" size Resolves #1438

--- componentLibraries/defaultLibrary/Hydraulic/Volumes&Lines/HydraulicHose.hpp
+++ componentLibraries/defaultLibrary/Hydraulic/Volumes&Lines/HydraulicHose.hpp
@@ -47,16 +47,10 @@ namespace hopsan {
         double RQ10, RQ20, RQF1D, RQF2D, RQEF1D, RQEF2D, C1F1, C2F1, RQ1, RL1, RQ2, RL2, RL;
         double W1, W2, W3, W4, C1F, C2F, RQF1, RQF2, RQEF1, RQEF2;
         FirstOrderTransferFunction FilterC1F, FilterC2F, FilterC1F1, FilterC2F1;
-        double numC1F[2], denC1F[2];
-        double numC2F[2], denC2F[2];
-        double numC1F1[2], denC1F1[2];
-        double numC2F1[2], denC2F1[2];
 
         // Cyclic memmory
-        //! @todo The memory size should not be hardcoded
         int NTIME, NTIME1, NTMAX;
-        double c1i[1001];       //1001 because 1000 with starting at 1 (stupid Fortran thing)
-        double c2i[1001];
+        double *mpC1i, *mpC2i;
 
         // Constants
         double betae, wallVisc;
@@ -86,6 +80,10 @@ namespace hopsan {
 
             addConstant("beta_e", "Bulk modulus",              "Pa",     1e9, betae);
             addConstant("eta_w",  "Equivalent wall viscosity", "Ns/m^2", 0.03, wallVisc);
+
+            // Make sure that buffer ptrs NULL by default
+            mpC1i=0;
+            mpC2i=0;
         }
 
 
@@ -114,7 +112,6 @@ namespace hopsan {
             d = (*mpD);
             l = (*mpL);
 
-            NTMAX = 1000;
             kappa = 1.25;
 
                 // Line resistance
@@ -130,11 +127,19 @@ namespace hopsan {
             TL = l/a;
             NT = TL/mTimestep+0.5;
 
+            // Allocate necessary storage based on NT value
+            NTMAX = int(NT+1);
+            // We need NTMAX+1 here to account for the "fortran index begins at 1 hack" introduced in the translation of the code to c++
+            //! @todo We should try to use indexing based on 0 (as it should be in c)
+            mpC1i = new double[NTMAX+1];
+            mpC2i = new double[NTMAX+1];
+
             TN = NT*mTimestep;
 
             area = pi*d*d/4.0;
             areaC = area*TL/TN;
             Zc0= rho*a/areaC;
+
             if(NT < 1) NT = 1;
             TN = NT*mTimestep;
 
@@ -150,8 +155,8 @@ namespace hopsan {
 
             for(int i=1; i<NTMAX+1; ++i)
             {
-                c1i[i] = p1 + Zc0*q1;
-                c2i[i] = p2 + Zc0*q2;
+                mpC1i[i] = p1 + Zc0*q1;
+                mpC2i[i] = p2 + Zc0*q2;
             }
 
             NTIME=1;
@@ -169,10 +174,11 @@ namespace hopsan {
             c1 = p1 - Zc1*q1;
             c2 = p2 - Zc2*q2;
 
-            FilterC1F.initialize(mTimestep, numC1F, denC1F, c1i[NTIME], c1i[NTIME]);        //! @todo Not sure if start values for filters are correct
-            FilterC2F.initialize(mTimestep, numC2F, denC2F, c2i[NTIME], c2i[NTIME]);
-            FilterC1F1.initialize(mTimestep, numC1F1, denC1F1, c1i[NTIME], C1F1);
-            FilterC2F1.initialize(mTimestep, numC2F1, denC2F1, c2i[NTIME], C2F1);
+            double dummy[2] = {0, 0};
+            FilterC1F.initialize(mTimestep, dummy, dummy, mpC1i[NTIME], mpC1i[NTIME]);        //! @todo Not sure if start values for filters are correct
+            FilterC2F.initialize(mTimestep, dummy, dummy, mpC2i[NTIME], mpC2i[NTIME]);
+            FilterC1F1.initialize(mTimestep, dummy, dummy, mpC1i[NTIME], C1F1);
+            FilterC2F1.initialize(mTimestep, dummy, dummy, mpC2i[NTIME], C2F1);
 
             //Write new values to nodes
             (*mpP1_c) = c1;
@@ -224,8 +230,8 @@ namespace hopsan {
             Zc1 = Zc0 + BC0*RL1;
             Zc2 = Zc0 + BC0*RL2;
 
-            c1i[NTIME] = c1 + 2.0*Zc0*q1;
-            c2i[NTIME] = c2 + 2.0*Zc0*q2;
+            mpC1i[NTIME] = c1 + 2.0*Zc0*q1;
+            mpC2i[NTIME] = c2 + 2.0*Zc0*q2;
 
                 // Low pass filtering of transmitted signals
             W1 = 1.0/(kappa*TN);
@@ -234,19 +240,22 @@ namespace hopsan {
             W4 = W3*exp(Rw/(2.0*Zc0));
 
 
+            double numC1F[2], denC1F[2], numC2F[2], denC2F[2];
+            double numC1F1[2], denC1F1[2], numC2F1[2], denC2F1[2];
+
             numC1F[1] = 1.0/W2;
             numC1F[0] = 1.0;
             denC1F[1] = 1.0/W1;
             denC1F[0] = 1.0;
             FilterC1F.setNumDen(numC1F, denC1F);
-            C1F = FilterC1F.update(c1i[NTIME1]);
+            C1F = FilterC1F.update(mpC1i[NTIME1]);
 
             numC2F[1] = 1.0/W2;
             numC2F[0] = 1.0;
             denC2F[1] = 1.0/W1;
             denC2F[0] = 1.0;
             FilterC2F.setNumDen(numC2F, denC2F);
-            C2F = FilterC2F.update(c2i[NTIME1]);
+            C2F = FilterC2F.update(mpC2i[NTIME1]);
 
             numC1F1[1] = 1.0/W4;
             numC1F1[0] = 1.0;
@@ -287,6 +296,18 @@ namespace hopsan {
             (*mpP2_c) = c2;
             (*mpP2_Zc) = Zc2;
 
+        }
+
+        void finalize()
+        {
+            if (mpC1i && mpC2i)
+            {
+                delete[] mpC1i;
+                delete[] mpC2i;
+                mpC1i = 0;
+                mpC2i = 0;
+                NTMAX = 0;
+            }
         }
 
 
