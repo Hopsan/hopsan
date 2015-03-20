a94c1e8ddc719d4b5c984f1dec708f6d769c1f23
Robert Braun <robert.braun@liu.se>
2015-03-20 06:58:14 +0000

    Fixed crash when loading duplicated connectors without point vectors.

--- HopsanGUI/loadFunctions.cpp
+++ HopsanGUI/loadFunctions.cpp
@@ -110,7 +110,7 @@ bool loadConnector(QDomElement &rDomElement, ContainerObject* pContainer, UndoSt
         Connector* pConn = pContainer->createConnector(startPort, endPort, NoUndo);
         if (pConn)
         {
-            if(pointVector.isEmpty())   //Create a diagonal connector if no points were loaded from HMF
+            if(pointVector.isEmpty() && !pConn->isDangling() && !pConn->isBroken())   //Create a diagonal connector if no points were loaded from HMF
             {
                 pointVector.push_back(pConn->getStartPort()->boundingRect().center());
                 pointVector.push_back(pConn->getEndPort()->boundingRect().center());
