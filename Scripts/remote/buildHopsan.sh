a774650ce78ba541211ce4332debe39e9a774103
Peter Nordin <peterNordin@users.noreply.github.com>
2015-10-26 12:28:20 +0000

    Help scripts for remote Hopsan installation

---
+++ Scripts/remote/buildHopsan.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd hopsantrunk
+cd HopsanCore
+qmake -qt=5
+make -j4 -B
+cd ..
+cd componentLibraries/defaultLibrary
+qmake -qt=5
+make -j2 -B
+cd ../../
+cd Dependencies
+./setupZMQandMSGPACK.sh
+./setupFMILibrary.sh
+cd ..
+cd SymHop
+qmake -qt=5
+make -j4 -B
+cd ..
+cd HopsanRemote
+qmake -qt=5
+make -j4 -B
+cd ..
+cd HopsanCLI
+qmake -qt=5
+make -j4 -B
+cd ..
+cd HopsanGenerator
+sed "s|useqtgui=True|useqtgui=False|g" -i HopsanGenerator.pro
+qmake -qt=5
+make -j4 -B
+cd ..
---
+++ Scripts/remote/installHopsanBuildDeps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+debs="subversion build-essential qt5-qmake qtbase5-dev  make libtbb-dev unzip libtool automake pkg-config cmake"
+sudo apt-get -y install $debs
+
