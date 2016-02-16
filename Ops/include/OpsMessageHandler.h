4e80ca6d9b8abfa36085c6fbeebd2ec93c91a996
Robert Braun <robert.braun@liu.se>
2016-02-16 10:11:07 +0000

    Added missing file.

---
+++ Ops/include/OpsMessageHandler.h
@@ -0,0 +1,40 @@
+#ifndef OPSMESSAGEHANDLER_H
+#define OPSMESSAGEHANDLER_H
+
+#include <sstream>
+
+namespace Ops {
+
+
+class MessageHandler
+{
+public:
+    MessageHandler() { mIsAborted = false;}
+
+    void printMessage(std::string msg)
+    {
+        std::stringstream ss;
+        ss << msg;
+        printMessage(ss.str().c_str());
+    }
+
+    virtual void printMessage(const char*) {}
+    virtual void pointChanged(size_t) {}
+    virtual void pointsChanged() {}
+    virtual void candidateChanged(size_t) {}
+    virtual void candidatesChanged() {}
+    virtual void objectiveChanged(size_t) {}
+    virtual void objectivesChanged() {}
+    virtual void stepCompleted(size_t) {}
+
+    bool aborted() { return mIsAborted; }
+
+    bool setAborted(const bool value) { mIsAborted = value; }
+
+protected:
+    bool mIsAborted;
+};
+
+}
+
+#endif // OPSMESSAGEHANDLER_H
