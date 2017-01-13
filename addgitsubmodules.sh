eb639bb6ac4cd37b880a79084803f3239867fe14
Peter Nordin <peterNordin@users.noreply.github.com>
2017-01-13 19:27:22 +0000

    Script to add git submoduls after migration. Adding them now wont work bacause I can not store actual checkot version  in subversion

---
+++ addgitsubmodules.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function addit {
+giturl=$1
+dir=$2
+co=$3
+
+echo $giturl $dir $co
+rm -rf $dir
+git submodule add $giturl $dir
+cd $dir
+git checkout $co
+cd ..
+
+}
+
+#git reset --hard HEAD
+
+
+addit https://github.com/msgpack/msgpack-c.git Dependencies/msgpack-c cpp-1.3.0
+addit https://github.com/zeromq/cppzmq.git Dependencies/cppzmq HEAD
+addit https://github.com/zeromq/zeromq4-1.git Dependencies/zeromq v4.1.6
+addit https://github.com/Orc/discount.git Dependencies/discount v2.1.8
+addit https://github.com/peterNordin/IndexingCSVParser.git Dependencies/IndexingCSVParser HEAD
+addit https://github.com/peterNordin/libNumHop.git Dependencies/libNumHop HEAD
+addit https://github.com/modelon/FMILibrary.git Dependencies/FMILibrary 86929c1a32ec4a5953107e49f0d60d1a48cfeef7
+addit https://github.com/tbeu/matio.git Dependencies/matio v1.5.2
+addit https://github.com/osakared/qwt.git Dependencies/qwt tags/qwt-6.1.3
+addit https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git Dependencies/hdf5 hdf5-1_8_18
+addit https://github.com/Khan/KaTeX.git Dependencies/KaTeX v0.7.0
+#addit Dependencies/
+#addit Dependencies/
+echo Done
