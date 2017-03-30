851a4fe48ea17de453cb536457a87ef829b91c81
Robert Braun <robert.braun@liu.se>
2017-03-30 12:55:23 +0000

    Removed springs and dry friction from Forks component and fixed several bugs. Resolves #1628.

--- componentLibraries/defaultLibrary/Special/TMMS04/Forks.hpp
+++ componentLibraries/defaultLibrary/Special/TMMS04/Forks.hpp
@@ -29,6 +29,8 @@
 #include "ComponentUtilities.h"
 #include <cmath>
 
+using namespace std;
+
 namespace hopsan {
 
     class Forks : public ComponentQ
@@ -36,20 +38,20 @@ namespace hopsan {
     private:
         //Constants
         double mR, mRrot, mM, mJ, mSl;
-        
-        
+
+
         //Output variable node data pointers
         double *mpX, *mpA;
         double *mpDebug;
-        
+
         //Input Variable node data pointers
-        double *mpB, *mpK, *mpXMin, *mpXMax;
-        double *mpKfrot, *mpBrot, *mpKrot, *mpAMin, *mpAMax;
-        
-        
+        double *mpB, *mpXMin, *mpXMax;
+        double *mpBrot, *mpAMin, *mpAMax;
+
+
         //Power port pointers
         Port *mpP1, *mpP2, *mpP3;
-        
+
         //Power port node data pointers
         double *mpP1_a, *mpP1_w, *mpP1_T, *mpP1_Je, *mpP1_c, *mpP1_Zc;
         double *mpP2_x, *mpP2_v, *mpP2_f, *mpP2_me, *mpP2_c, *mpP2_Zc;
@@ -60,13 +62,13 @@ namespace hopsan {
         FirstOrderTransferFunction mFilterV;
         double mNumX[3], mNumV[2];
         double mDenX[3], mDenV[2];
-        
+
         // Other member variables
         SecondOrderTransferFunction mFilterA;
         FirstOrderTransferFunction mFilterW;
         double mNumA[3], mNumW[2];
         double mDenA[3], mDenW[2];
-        
+
     public:
         static Component *Creator()
         {
@@ -75,26 +77,23 @@ namespace hopsan {
 
         void configure()
         {
-            
+
             addConstant("r", "Radius (traverse)", "", 0.1, mR);
             addConstant("m", "Mass (traverse)", "kg",                      100.0, mM);
             addConstant("sl", "Piston strokes", "m", 1, mSl);
             addInputVariable("B", "Viscous Friction (traverse)", "Ns/m",   10.0, &mpB);
-            addInputVariable("k", "Spring Coefficient (traverse)", "N/m", 0.0, &mpK);
-            addInputVariable("x_min", "Minimum Position (traverse)", "m", -0.6, &mpXMin);
+             addInputVariable("x_min", "Minimum Position (traverse)", "m", -0.6, &mpXMin);
             addInputVariable("x_max", "Maximum Position (traverse)", "m", 0.6, &mpXMax);
             addOutputVariable("x", "Position (traverse)", "m", -0.6, &mpX);
-            
+
             addConstant("r2", "Radius (rotation)", "", 0.1, mRrot);
             addConstant("J", "Inertia (rotation)", "kgm^2",       0.2, mJ);
-            addInputVariable("Kfrot", "Dry Friction (rotation)", "Nm",   10.0, &mpKfrot);
             addInputVariable("Brot", "Viscous Friction (rotation)", "Nms/rad",   10.0, &mpBrot);
-            addInputVariable("krot", "Spring Coefficient (rotation)", "N/m", 0.0, &mpKrot);
             addInputVariable("a_min", "Minimum Angle (rotation)", "rad", -1.5707963267949, &mpAMin);
             addInputVariable("a_max", "Maximum Angle (rotation)", "rad", 1.5707963267949, &mpAMax);
             addOutputVariable("a", "Angle (rotation)", "rad", 1.5707963267949, &mpA);
             addOutputVariable("debug", "Debug", "", 0, &mpDebug);
-            
+
             //Add power ports
             mpP1 = addPowerPort("P1", "NodeMechanicRotational", "");
             mpP2 = addPowerPort("P2", "NodeMechanic", "");
@@ -111,7 +110,7 @@ namespace hopsan {
             mpP1_Je = getSafeNodeDataPtr(mpP1, NodeMechanicRotational::EquivalentInertia);
             mpP1_c = getSafeNodeDataPtr(mpP1, NodeMechanicRotational::WaveVariable);
             mpP1_Zc = getSafeNodeDataPtr(mpP1, NodeMechanicRotational::CharImpedance);
-            
+
             mpP2_x = getSafeNodeDataPtr(mpP2, NodeMechanic::Position);
             mpP2_v = getSafeNodeDataPtr(mpP2, NodeMechanic::Velocity);
             mpP2_f = getSafeNodeDataPtr(mpP2, NodeMechanic::Force);
@@ -129,20 +128,20 @@ namespace hopsan {
             //Read variable values from nodes
             const double t1 = (*mpP1_a);
             const double r = mR;
-            
+
             const double f2 = (*mpP2_f);
             const double f3 = (*mpP3_f);
             const double r2 = mRrot;
 
             const double x = (*mpX);
             const double a = (*mpA);
-                        
+
             //WRITE YOUR INITIALIZATION CODE HERE
 
             mNumX[0] = 1.0;
             mNumX[1] = 0.0;
             mNumX[2] = 0.0;
-            mDenX[0] = (*mpK);
+            mDenX[0] = 0;
             mDenX[1] = (*mpB);
             mDenX[2] = mM;
             mNumV[0] = 1.0;
@@ -152,11 +151,11 @@ namespace hopsan {
 
             mFilterX.initialize(mTimestep, mNumX, mDenX, t1/r, x, -1.5e300, 1.5e300, x);
             mFilterV.initialize(mTimestep, mNumV, mDenV, t1/r, 0);
-            
+
             mNumA[0] = 1.0;
             mNumA[1] = 0.0;
             mNumA[2] = 0.0;
-            mDenA[0] = (*mpKrot);
+            mDenA[0] = 0;
             mDenA[1] = (*mpBrot);
             mDenA[2] = mJ;
             mNumW[0] = 1.0;
@@ -165,11 +164,11 @@ namespace hopsan {
             mDenW[1] = mJ;
 
             mFilterA.initialize(mTimestep, mNumA, mDenA, (f2-f3)*r2, a, -1.5e300, 1.5e300, a);
-            mFilterW.initialize(mTimestep, mNumW, mDenW, (f2-f3)*r2 - (*mpKrot)*a, 0);
-            
+            mFilterW.initialize(mTimestep, mNumW, mDenW, (f2-f3)*r2, 0);
+
             const double x2 = a*r2-mSl/2;
             const double x3 = a*r2-mSl/2;
-                        
+
             (*mpP2_x) = x2;
             (*mpP3_x) = x3;
         }
@@ -178,36 +177,32 @@ namespace hopsan {
         void simulateOneTimestep()
         {
             double t1, a1, w1, x, v;
-            double f2, x2, v2, f3, x3=0, v3, a, w;
+            double f2, x2=0, v2, f3, x3=0, v3, a, w;
 
             //Get variable values from nodes
             const double r = mR;
             const double c1 = (*mpP1_c);
             const double Zx1 = (*mpP1_Zc);
-            const double k = (*mpK);
             const double B = (*mpB);
-            
+
             const double r2 = mRrot;
             const double c2 = (*mpP2_c);
             const double Zx2 = (*mpP2_Zc);
             const double c3 = (*mpP3_c);
             const double Zx3 = (*mpP3_Zc);
-            const double krot = (*mpKrot);
             const double Brot = (*mpBrot);
-            const double Kfrot = (*mpKfrot);
 
             a1 = (*mpP1_a);
             w1 = (*mpP1_a);
 
             //Traverse equations
-            mDenX[0] = k;
-            mDenX[1] = B+Zx1;
-            mDenV[0] = B+Zx1;
+            mDenX[1] = B+Zx1/r/r;
+            mDenV[0] = B+Zx1/r/r;
             mFilterX.setDen(mDenX);
             mFilterV.setDen(mDenV);
 
             x = mFilterX.update(c1/r);
-            v = mFilterV.update(c1/r - k*a1*r);
+            v = mFilterV.update(c1/r);
 
             if(x<(*mpXMin))
             {
@@ -227,33 +222,29 @@ namespace hopsan {
             a1 = -x/r;
             w1 = -v/r;
             t1 = c1 + Zx1*w1;
-            
-            //Rotation equations
-            double F_dry = std::max(Kfrot*tanh(10000*(*mpP2_v)/r2),(c2-c3)*r2);
-            (*mpDebug) = F_dry;
 
-            mDenA[0] = krot;
-            mDenA[1] = Brot+Zx2+Zx3;
-            mDenW[0] = Brot+Zx2+Zx3;
+            //Rotation equations
+            mDenA[1] = Brot+Zx2*r2*r2+Zx3*r2*r2;
+            mDenW[0] = Brot+Zx2*r2*r2+Zx3*r2*r2;
             mFilterA.setDen(mDenA);
             mFilterW.setDen(mDenW);
 
-            a = mFilterA.update((c2-c3)*r2 + F_dry);
-            w = mFilterW.update((c2-c3)*r2 + F_dry - krot*x3/r2);
+            a = mFilterA.update((c2-c3)*r2);
+            w = mFilterW.update((c2-c3)*r2);
 
             if(a<(*mpAMin))
             {
                 a=(*mpAMin);
                 w=0.0;
-                mFilterA.initializeValues((c2-c3)*r2 + F_dry, a);
-                mFilterW.initializeValues((c2-c3)*r2 + F_dry, 0.0);
+                mFilterA.initializeValues((c2-c3)*r2, a);
+                mFilterW.initializeValues((c2-c3)*r2, 0.0);
             }
             if(a>(*mpAMax))
             {
                 a=(*mpAMax);
                 w=0.0;
-                mFilterA.initializeValues((c2-c3)*r2 + F_dry, a);
-                mFilterW.initializeValues((c2-c3)*r2 + F_dry, 0.0);
+                mFilterA.initializeValues((c2-c3)*r2, a);
+                mFilterW.initializeValues((c2-c3)*r2, 0.0);
             }
 
             x2 = -a*r2-mSl/2;
@@ -263,22 +254,22 @@ namespace hopsan {
             x3 = a*r2-mSl/2;
             v3 = w*r2;
             f3 = c3 + Zx3*v3;
-            
+
             //Write new values to nodes
             (*mpP1_T) = t1;
             (*mpP1_a) = a1;
             (*mpP1_w) = w1;
-            
+
             (*mpX) = x;
-            
+
             (*mpP2_f) = f2;
             (*mpP2_x) = x2;
             (*mpP2_v) = v2;
-            
+
             (*mpP3_f) = f3;
             (*mpP3_x) = x3;
             (*mpP3_v) = v3;
-            
+
             (*mpA) = a;
         }
 
@@ -297,5 +288,4 @@ namespace hopsan {
 }
 
 #endif //FORKS_HPP_INCLUDED
-
 
