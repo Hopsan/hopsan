e81d9f3ad97ddad816ea95aa9dd0e87bef742bce
Peter Nordin <peterNordin@users.noreply.github.com>
2017-05-27 21:49:00 +0000

    Update script

--- updateLicenseHeaders.sh
+++ updateLicenseHeaders.sh
@@ -10,10 +10,10 @@ DOSET='--set'
 #DOSET=''
 
 LIC_HEADER='licenseHeaderAPL2'
-Utilities/licenseChanger.py HopsanCore ${LIC_HEADER} -e HopsanCore/dependencies ${DOSET}
+Utilities/licenseChanger.py HopsanCore ${LIC_HEADER} ${DOSET} -e HopsanCore/dependencies
 Utilities/licenseChanger.py HopsanCLI ${LIC_HEADER} ${DOSET}
 Utilities/licenseChanger.py HopsanRemote ${LIC_HEADER} ${DOSET}
-Utilities/licenseChanger.py HopsanGenerator ${LIC_HEADER} ${DOSET}
+Utilities/licenseChanger.py HopsanGenerator ${LIC_HEADER} ${DOSET} -e HopsanGenerator/templates
 Utilities/licenseChanger.py Ops ${LIC_HEADER} ${DOSET}
 Utilities/licenseChanger.py SymHop ${LIC_HEADER} ${DOSET}
 Utilities/licenseChanger.py componentLibraries/defaultLibrary ${LIC_HEADER} ${DOSET}
@@ -24,5 +24,5 @@ Utilities/licenseChanger.py componentLibraries/extensionLibrary08 ${LIC_HEADER} 
 Utilities/licenseChanger.py UnitTests ${LIC_HEADER} ${DOSET}
 
 LIC_HEADER='licenseHeaderGPL3'
-Utilities/licenseChanger.py HopsanGUI ${LIC_HEADER} -e HopsanGUI/Dependencies ${DOSET}
+Utilities/licenseChanger.py HopsanGUI ${LIC_HEADER} ${DOSET} -e HopsanGUI/Dependencies
 Utilities/licenseChanger.py HoLC ${LIC_HEADER} ${DOSET}
