e210a72008580da370171dac889cc35a67af44ca
Peter Nordin <peterNordin@users.noreply.github.com>
2017-06-06 09:11:09 +0200

    Update default license header file for auto generated files

--- componentLibraries/extensionLibrary/generateLibraryFiles.py
+++ componentLibraries/extensionLibrary/generateLibraryFiles.py
@@ -321,8 +321,8 @@ if __name__ == "__main__":
         exit()
     elif len(sys.argv) < 3:
         rootDir = sys.argv[1]
-        print('Warning: No license file given, using: ../../licenseHeader')
-        licenseheader = readFile('../../licenseHeader')
+        print('Warning: No license file given, using: ../../licenseHeaderAPL2')
+        licenseheader = readFile('../../licenseHeaderAPL2')
     else:
         rootDir = sys.argv[1]
         licenseheader = readFile(sys.argv[2])
