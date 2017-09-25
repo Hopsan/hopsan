856b8da80fc429f3c8f4cd75ff5a21bbf6ab681d
Peter Nordin <peterNordin@users.noreply.github.com>
2017-09-25 12:38:28 +0200

    Change link for hopsanreleases xml and remove obsolete autoupdate link

--- HopsanGUI/common.h
+++ HopsanGUI/common.h
@@ -41,10 +41,9 @@
 
 // Web link definitions
 #define HOPSANLINK "http://tiny.cc/hopsan"
-#define VERSIONLINK "http://tiny.cc/hopsannews"
+#define VERSIONLINK "http://tiny.cc/hopsanreleases"
 #define NEWSLINK "http://tiny.cc/hopsannewsfeed"
 #define DOWNLOADLINK "http://tiny.cc/hopsanarchive"
-#define AUTOUPDATELINK "http://tiny.cc/hopsanupdate"
 
 // Path definitions (development and release)
 // qrc paths
