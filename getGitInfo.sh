4c7166b808b9ee1c6a109dc13ce61743c8a94f42
Peter Nordin <peterNordin@users.noreply.github.com>
2017-09-29 14:48:43 +0200

    Exit with error code on failure

--- getGitInfo.sh
+++ getGitInfo.sh
@@ -10,18 +10,24 @@ elif [ -f "$2" ]; then
   dir="$2"
 else
   echo Error $2 is not a file or directory
+  exit 1
 fi
 
 if [ "$1" == "shorthash" ]; then
   git log -n1 --pretty=format:%h -- $dir
+  exit $?
 elif [ "$1" == "date" ]; then
   date -d @`git log -n1 --pretty=format:%ct -- $dir` +%Y%m%d
+  exit $?
 elif [ "$1" == "date.time" ]; then
   commitdate=$(date -d @`git log -n1 --pretty=format:%ct -- $dir` +%Y%m%d)
   committime=$(date -d @`git log -n1 --pretty=format:%ct -- $dir` +%H%M)
+  ret_code=$?
   echo ${commitdate}.${committime}
+  exit ${ret_code}
 else
   echo Incorrect request: $1
+  exit 1
 fi
 
 
