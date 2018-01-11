a2d0c6764151f89356410bc18681e96cbc8d9bbd
Peter Nordin <peterNordin@users.noreply.github.com>
2018-01-11 21:34:42 +0000

    Update and cleanup code preparation script

--- buildDebPackage/prepareSourceCode.sh
+++ buildDebPackage/prepareSourceCode.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # $Id$
 
-# Shell script for exporting and preparing the Hopsan source code before RELEASE build
+# Shell script for preparing the Hopsan source code before RELEASE build
 # Author: Peter Nordin peter.nordin@liu.se
-# For use in Hopsan, requires "subversion command line" installed (apt-get install subversion)
 
 git_export_all()
 {
@@ -14,7 +13,7 @@ git_export_all()
   echo "Exporting from git: ${src} to ${dst}"
   mkdir -p ${dst}
   pushd ${src}
-  ${hopsanroot}/Dependencies/git-archive-all/git_archive_all.py ${tarfile}
+  ${src_hopsan_root}/Dependencies/git-archive-all/git_archive_all.py ${tarfile}
   popd
   mv ${src}/${tarfile} ${dst}
   pushd ${dst}
@@ -26,62 +25,62 @@ git_export_all()
 
 
 E_BADARGS=65
-if [ $# -lt 5 ]; then
+if [ $# -lt 7 ]; then
   echo "Error: To few input arguments!"
-  echo "Usage: `basename $0` {srcDir dstDir baseversion releaserevision fullversionname doDevRelease doBuildInComponents}"
+  echo "Usage: `basename $0` {src_hopsan_root dst_directory base_version release_revision full_version_string doDevRelease doBuildInComponents}"
   exit $E_BADARGS
 fi
 
-srcDir="$1"
-hopsanroot="$1"
-dstDir="$2"
-baseversion="$3"
-releaserevision="$4"
-fullversionname="$5"
-doDevRelease="$6"
-doBuildInComponents="$7"
+readonly src_hopsan_root="$1"
+readonly dst_directory="$2"
+readonly base_version="$3"
+readonly release_revision="$4"
+readonly full_version_string="$5"
+readonly doDevRelease="$6"
+readonly doBuildInComponents="$7"
 
 # -----------------------------------------------------------------------------
-# Determine the Core Gui and CLI svn rev numbers for this release
+# Determine the Core Gui and CLI revision numbers and hashes
 #
-cd ${srcDir}/HopsanCore; corecommitdt=$(../getGitInfo.sh date.time .); cd $OLDPWD
-cd ${srcDir}/HopsanCore; corecommithash=$(../getGitInfo.sh shorthash .); cd $OLDPWD
-cd ${srcDir}/HopsanGUI; guicommitdt=$(../getGitInfo.sh date.time .); cd $OLDPWD
-cd ${srcDir}/HopsanCLI; clicommitdt=$(../getGitInfo.sh date.time .); cd $OLDPWD
+cd ${src_hopsan_root}/HopsanCore; corecommitdt=$(../getGitInfo.sh date.time .); cd $OLDPWD
+cd ${src_hopsan_root}/HopsanCore; corecommithash=$(../getGitInfo.sh shorthash .); cd $OLDPWD
+cd ${src_hopsan_root}/HopsanGUI; guicommitdt=$(../getGitInfo.sh date.time .); cd $OLDPWD
+cd ${src_hopsan_root}/HopsanCLI; clicommitdt=$(../getGitInfo.sh date.time .); cd $OLDPWD
 echo "Core_CDT: ${corecommitdt}, GUI_CDT: ${guicommitdt}, CLI_CDT: ${clicommitdt}"
 
 # -----------------------------------------------------------------------------
-# Export source dirs and files
+# Export source code for a clean build, unless src and dst directories are the same 
 #
-echo "Exporting $srcDir to $dstDir for preparation"
-rm -rf ${dstDir}
-git_export_all ${srcDir} ${dstDir}
-
+if [[ ${src_hopsan_root} = ${dst_directory} ]]; then
+  echo "Source and destination are the same. Not exporting!"
+else
+  echo "Exporting $src_hopsan_root to $dst_directory for preparation"
+  rm -rf ${dst_directory}
+  git_export_all ${src_hopsan_root} ${dst_directory}
+fi
 
 # -----------------------------------------------------------------------------
-# Prepare files
+# Prepare files in destination directory
 #
-cd ${dstDir}
-
-# Clean bin folder
-#rm -rf ./bin/*
+cd ${dst_directory}
 
 # Generate default library files
 cd componentLibraries/defaultLibrary; ./generateLibraryFiles.py .; cd $OLDPWD
 
-# Set the Core Gui and CLI svn rev numbers for this release
+# Set the Core Gui and CLI rev numbers, hashes and release version for this release
 sed "s|#define HOPSANCORE_COMMIT_HASH.*|#define HOPSANCORE_COMMIT_HASH ${corecommithash}|g" -i HopsanCore/include/HopsanCoreGitVersion.h
 sed "s|#define HOPSANCORE_COMMIT_TIMESTAMP.*|#define HOPSANCORE_COMMIT_TIMESTAMP ${corecommitdt}|g" -i HopsanCore/include/HopsanCoreGitVersion.h
 sed "s|#define HOPSANGUI_COMMIT_TIMESTAMP.*|#define HOPSANGUI_COMMIT_TIMESTAMP ${guicommitdt}|g" -i HopsanGUI/version_gui.h
 sed "s|#define HOPSANCLI_COMMIT_TIMESTAMP.*|#define HOPSANCLI_COMMIT_TIMESTAMP ${clicommitdt}|g" -i HopsanCLI/version_cli.h
 
-sed "s|#define HOPSANRELEASEVERSION.*|#define HOPSANRELEASEVERSION \"${fullversionname}\"|g" -i HopsanGUI/version_gui.h
+sed "s|#define HOPSANRELEASEVERSION.*|#define HOPSANRELEASEVERSION \"${full_version_string}\"|g" -i HopsanGUI/version_gui.h
 
-if [ $doDevRelease = "false" ]; then
-  # Set version numbers (by changing .h files) BEFORE build
-  sed "s|#define HOPSANCOREVERSION.*|#define HOPSANCOREVERSION \"${fullversionname}\"|g" -i HopsanCore/include/HopsanCoreVersion.h
-  sed "s|#define HOPSANGUIVERSION.*|#define HOPSANGUIVERSION \"${fullversionname}\"|g" -i HopsanGUI/version_gui.h
-  sed "s|#define HOPSANCLIVERSION.*|#define HOPSANCLIVERSION \"${fullversionname}\"|g" -i HopsanCLI/version_cli.h
+# Handle official release preparation
+if [[ $doDevRelease = "false" ]]; then
+  # Overwrite version numbers for official release so that allcomponents show the same version
+  sed "s|#define HOPSANCOREVERSION.*|#define HOPSANCOREVERSION \"${full_version_string}\"|g" -i HopsanCore/include/HopsanCoreVersion.h
+  sed "s|#define HOPSANGUIVERSION.*|#define HOPSANGUIVERSION \"${full_version_string}\"|g" -i HopsanGUI/version_gui.h
+  sed "s|#define HOPSANCLIVERSION.*|#define HOPSANCLIVERSION \"${full_version_string}\"|g" -i HopsanCLI/version_cli.h
 
   # Hide splash screen development warning
   sed "s|Development version||g" -i HopsanGUI/graphics/splash2.svg
@@ -91,12 +90,13 @@ if [ $doDevRelease = "false" ]; then
 fi
 
 # Set splash screen version number
-sed "s|0\.0\.0|$baseversion|g" -i HopsanGUI/graphics/splash2.svg
-sed "s|20170000\.0000|$releaserevision|g" -i HopsanGUI/graphics/splash2.svg
+sed "s|0\.0\.0|$base_version|g" -i HopsanGUI/graphics/splash2.svg
+sed "s|20170000\.0000|$release_revision|g" -i HopsanGUI/graphics/splash2.svg
 inkscape ./HopsanGUI/graphics/splash2.svg --export-background=rgb\(255,255,255\) --export-png ./HopsanGUI/graphics/splash.png
 
-# Make sure we compile defaultLibrary into core
-if [ "$doBuildInComponents" = "true" ]; then
+# If selected, make changes to compile defaultLibrary into Hopsan Core
+# Deprecated, we should not do this anymore
+if [[ "$doBuildInComponents" = "true" ]]; then
   sed 's|.*DEFINES \*= BUILTINDEFAULTCOMPONENTLIB|DEFINES *= BUILTINDEFAULTCOMPONENTLIB|g' -i Common.prf
   sed 's|#INTERNALCOMPLIB.CC#|../componentLibraries/defaultLibrary/defaultComponentLibraryInternal.cc \\|g' -i HopsanCore/HopsanCore.pro
   sed '/.*<lib>.*/d' -i componentLibraries/defaultLibrary/defaultComponentLibrary.xml
