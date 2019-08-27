238eac994e7f55126eb8dac5050f43ea50fe6539
Magnus Sethson <magnus@sethson.se>
2019-08-27 09:00:25 +0200

    Update getGitInfo.sh for macos

--- getGitInfo.sh
+++ getGitInfo.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 
-if [ $# -lt 2 ]; then
+if [[ $# -lt 2 ]]; then
   dir="."
 fi
 
-if [ -d "$2" ]; then
+if [[ -d "$2" ]]; then
   dir="$2"
-elif [ -f "$2" ]; then
+elif [[ -f "$2" ]]; then
   dir="$2"
 else
   echo Error $2 is not a file or directory
   exit 1
 fi
 
-if [ "$1" == "shorthash" ]; then
+if [[ "$1" == "shorthash" ]]; then
   git log -n1 --pretty=format:%h -- $dir
   exit $?
-elif [ "$1" == "date" ]; then
-  date -d @`git log -n1 --pretty=format:%ct -- $dir` +%Y%m%d
-  exit $?
-elif [ "$1" == "date.time" ]; then
-  commitdate=$(date -d @`git log -n1 --pretty=format:%ct -- $dir` +%Y%m%d)
-  committime=$(date -d @`git log -n1 --pretty=format:%ct -- $dir` +%H%M)
-  ret_code=$?
-  echo ${commitdate}.${committime}
-  exit ${ret_code}
+elif [[ "$1" == "date" ]]; then
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    # date is one of those system commands that differes on BSD-clones, /magse
+    date -j -f %s `git log -n1 --pretty=format:%ct -- $dir` +%Y%m%d
+    exit $?
+  else
+    date -d @`git log -n1 --pretty=format:%ct -- $dir` +%Y%m%d
+    exit $?
+  fi
+elif [[ "$1" == "date.time" ]]; then
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    commitdate=$(date -j -f %s `git log -n1 --pretty=format:%ct -- $dir` +%Y%m%d)
+    committime=$(date -j -f %s `git log -n1 --pretty=format:%ct -- $dir` +%H%M)
+    ret_code=$?
+    echo ${commitdate}.${committime}
+    exit ${ret_code}
+  else
+    commitdate=$(date -d @`git log -n1 --pretty=format:%ct -- $dir` +%Y%m%d)
+    committime=$(date -d @`git log -n1 --pretty=format:%ct -- $dir` +%H%M)
+    ret_code=$?
+    echo ${commitdate}.${committime}
+    exit ${ret_code}
+  fi
 else
   echo Incorrect request: $1
   exit 1
@@ -36,4 +50,3 @@ fi
 #echo ${commitdate//-/}
 #git log -n1 --pretty=format:%cd -- $dir
 #date -d @`git log -n1 --pretty=format:%ct -- $dir` +%Y%m%d%H%M
-
