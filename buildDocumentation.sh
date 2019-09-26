0c63406012d81ae5d75d26ee0902dcc1dc63ee65
Robert Braun <robert.braun@liu.se>
2019-09-26 14:01:22 +0200

    Shell script for copying documentation images to HTML directory

--- buildDocumentation.sh
+++ buildDocumentation.sh
@@ -6,6 +6,11 @@
 # Date:   2011-11-28
 
 cd doc
+
+files=$(find graphics -type f) && files=$(echo $files)
+sed "s@HTML_EXTRA_FILES       = @HTML_EXTRA_FILES       = ${files}@" -i Doxyfile
+sed "s@HTML_EXTRA_FILES       = @HTML_EXTRA_FILES       = ${files}@" -i Doxyfile_full
+
 if [ "$1" = "full" ]; then
     echo "Building full documentation"
     doxygen Doxyfile_full
