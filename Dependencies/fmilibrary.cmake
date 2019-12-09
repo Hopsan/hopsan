74441a3bb6815280e2455abc267db44da9e94884
Peter Nordin <jpn.peter@gmail.com>
2019-12-09 22:14:58 +0100

    Add IMPORTED_IMPLIB to fmilibrary for Windows CMake build

--- Dependencies/fmilibrary.cmake
+++ Dependencies/fmilibrary.cmake
@@ -4,3 +4,7 @@ add_library(fmilibrary SHARED IMPORTED)
 set_target_properties(fmilibrary PROPERTIES
     IMPORTED_LOCATION ${fmi_home}/lib/libfmilib_shared${CMAKE_SHARED_LIBRARY_SUFFIX}
     INTERFACE_INCLUDE_DIRECTORIES ${fmi_home}/include)
+if(WIN32)
+  set_target_properties(fmilibrary PROPERTIES
+    IMPORTED_IMPLIB ${fmi_home}/lib/libfmilib_shared${CMAKE_IMPORT_LIBRARY_SUFFIX})
+endif()
