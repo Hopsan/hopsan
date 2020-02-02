acdcf20417722191a2799b82e218f2ea44f119c2
Peter Nordin <jpn.peter@gmail.com>
2020-02-02 20:35:39 +0100

    Update ZeroMQ to 4.3.3 (almost) and cppzmq to 4.6

--- Dependencies/setupZeroMQ.bat
+++ Dependencies/setupZeroMQ.bat
@@ -14,13 +14,23 @@ set installdir=%basedir%\%name%
 REM Setup PATH
 call setHopsanBuildPaths.bat
 
+REM Apply build patch
+set PATH=%PATH_WITH_MSYS%
+cd %codedir%
+patch.exe --forward -p1 < ..\zeromq-Prevent-conflicting-definition-of-close-when-compili.patch
+set PATH=%PATH_WITHOUT_MSYS%
+
 REM build
+if exist %builddir% (
+  echo Removing existing build directory %builddir%
+  rmdir /S /Q %builddir%
+)
 mkdir %builddir%
 cd %builddir%
-cmake -Wno-dev -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_INSTALL_PREFIX=%installdir% %codedir%
-mingw32-make SHELL=cmd -j8
-mingw32-make SHELL=cmd install
-REM mingw32-make test
+cmake -Wno-dev -DWITH_LIBSODIUM=OFF -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%installdir% %codedir%
+mingw32-make -j8
+mingw32-make install
+mingw32-make test
 
 REM Now "install" cppzmq (header only), to the zmq install dir
 REM TODO in the future use cmake to install cppzmq, but that does not work right now since no "zeromq-config.cmake" file seem to be created.
