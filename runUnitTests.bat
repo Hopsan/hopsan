b83aef25bf994ac78776eaf64a9eac3bd39eacf5
Peter Nordin <jpn.peter@gmail.com>
2020-05-06 17:23:02 +0200

    Run HopsanGUI tests offscreen in CI
    Also run all tests from script even on failure

--- runUnitTests.bat
+++ runUnitTests.bat
@@ -4,6 +4,7 @@ SETLOCAL EnableDelayedExpansion
 set failed=0
 
 cd bin
+REM Run test programs
 for /r "." %%a in (tst_*.exe) do (
   "%%~fa"
   if !errorlevel! neq 0 (
@@ -11,6 +12,14 @@ for /r "." %%a in (tst_*.exe) do (
     set failed=1
   )
 )
+
+REM Run HopsanGUI built-in tests
+hopsangui.exe --test --platform offscreen
+if !errorlevel! neq 0 (
+  echo HopsanGUI test FAILED
+  set failed=1
+)
+
 cd ..
 if !failed! equ 1 (
   echo ERROR: At least one unit test failed!
--- runUnitTests.sh
+++ runUnitTests.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
-set -e
 set -u
 
 cd bin
+# Run test programs
+numfailed=0
 tests=$(ls tst_*)
 for test in ${tests}; do
     ./${test}
+    if [[ $? -ne 0 ]]; then
+        ((numfailed++))
+    fi
 done
-echo All tests passed
+
+# Run HopsanGUI built-in tests
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    # TODO make this work
+    echo Warning: HopsanGUI tests disabled on macOS Travis CI
+else
+   ./hopsangui --test --platform offscreen
+fi
+
+if [[ $? -ne 0 ]]; then
+    ((numfailed++))
+fi
+
+if [[ ${numfailed} -eq 0 ]]; then
+    echo
+    echo All tests passed
+    exit 0
+else
+    echo
+    echo ${numfailed} test applications failed
+    exit 1
+fi
