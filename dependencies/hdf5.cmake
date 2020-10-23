4ab254ce5cfe600ec6aff2bbbd4897e9ae6c1f44
Peter Nordin <jpn.peter@gmail.com>
2020-10-23 21:18:56 +0200

    Enable lookup of hdf5 and msgpack system installation

--- dependencies/hdf5.cmake
+++ dependencies/hdf5.cmake
@@ -1,8 +1,36 @@
 set(local_hdf5_dir ${CMAKE_CURRENT_LIST_DIR}/hdf5)
-find_package(Hdf5 CONFIG COMPONENTS C CXX PATHS ${local_hdf5_dir}/cmake ${local_hdf5_dir}/share/cmake)
+find_package(Hdf5 CONFIG COMPONENTS C CXX PATHS ${local_hdf5_dir}/cmake ${local_hdf5_dir}/share/cmake NO_DEFAULT_PATH)
+if (NOT Hdf5_FOUND)
+  message(STATUS "Looking for CMake Hdf5 in system")
+  find_package(Hdf5 CONFIG COMPONENTS C CXX)
+  if (NOT Hdf5_FOUND)
+    # Fall back to FindHDF5 Module
+    message(STATUS "Looking for HDF5 Module in system")
+    find_package(HDF5 COMPONENTS C CXX)
+  endif()
+endif()
+
 if (Hdf5_FOUND)
   message(STATUS "Building with HDF5 support")
   target_compile_definitions(hdf5::hdf5-shared INTERFACE USEHDF5)
+elseif(HDF5_FOUND)
+  # Repackage results from FindHDF5 Module as target
+  message(STATUS "Building with HDF5 support")
+  if (NOT TARGET module_hdf5-shared)
+    add_library(module_hdf5-shared INTERFACE)
+    add_library(module_hdf5_cpp-shared INTERFACE)
+    set_target_properties(module_hdf5-shared PROPERTIES
+      INTERFACE_LINK_LIBRARIES "${HDF5_C_LIBRARIES}"
+      INTERFACE_INCLUDE_DIRECTORIES "${HDF5_C_INCLUDE_DIRS}"
+      INTERFACE_COMPILE_DEFINITIONS "${HDF5_c_DEFINITIONS}")
+    add_library(hdf5::hdf5-shared ALIAS module_hdf5-shared)
+    set_target_properties(module_hdf5_cpp-shared PROPERTIES
+      INTERFACE_LINK_LIBRARIES "${HDF5_CXX_LIBRARIES}"
+      INTERFACE_INCLUDE_DIRECTORIES "${HDF5_CXX_INCLUDE_DIRS}"
+      INTERFACE_COMPILE_DEFINITIONS "${HDF5_CXX_DEFINITIONS}")
+    target_compile_definitions(module_hdf5_cpp-shared INTERFACE USEHDF5)
+    add_library(hdf5::hdf5_cpp-shared ALIAS module_hdf5_cpp-shared)
+  endif()
 else()
   message(WARNING "Building without HDF5 support")
 endif()
--- dependencies/msgpack.cmake
+++ dependencies/msgpack.cmake
@@ -1,3 +1,9 @@
 set(local_msgpack_dir ${CMAKE_CURRENT_LIST_DIR}/msgpack-c)
-find_package(msgpack CONFIG REQUIRED PATHS ${local_msgpack_dir} NO_DEFAULT_PATH) # Search for local version
-find_package(msgpack CONFIG REQUIRED) # Search in system
+find_package(msgpack CONFIG PATHS ${local_msgpack_dir} NO_DEFAULT_PATH) # Search for local version
+if (NOT msgpack_FOUND)
+  message(STATUS "Looking for msgpack in system")
+  find_package(msgpack CONFIG) # Search in system
+  if (msgpack_FOUND)
+    message(STATUS "  Found msgpack")
+  endif()
+endif()
