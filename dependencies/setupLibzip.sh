a4b592ab37389d3fec7c275161d2eac82c784484
Robert Braun <robert.braun@liu.se>
2021-01-07 10:57:42 +0100

    libzip dependency

---
+++ dependencies/setupLibzip.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Shell script building Hopsan dependency Libzip
+
+basedir=$(pwd)
+
+name=libzip
+codedir=${basedir}/${name}-code
+builddir=${basedir}/${name}-build
+installdir=${basedir}/${name}
+
+# Download and verify
+./download-dependencies.py ${name}
+
+# Include general settings
+source setHopsanBuildPaths.sh
+
+# Create build dir and enter it
+mkdir -p $builddir
+cd $builddir
+
+# Generate makefiles
+cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=$installdir ${codedir}
+
+# Build and install
+make -j16
+make install
+
+# Return to basedir
+cd $basedir
+echo "setupLibzip.sh done!"
