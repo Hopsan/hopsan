3c7970e6dc3f0c4d656961f5b93d367935888b0d
Robert Braun <robert.braun@liu.se>
2021-01-11 08:36:52 +0100

    Execute DCP slave simualtions from hopsandcp

--- hopsandcp/dcpslave.h
+++ hopsandcp/dcpslave.h
@@ -10,6 +10,9 @@ class ComponentSystem;
 }
 
 struct SlaveDescription_t;
+class DcpManagerSlave;
+class UdpDriver;
+class OstreamLog;
 
 class DcpSlave
 {
@@ -18,15 +21,28 @@ public:
     ~DcpSlave();
 
     void generateDescriptionFile(std::string &targetFile);
+    void start();
 
 private:
+    void printWaitingMessages();
+    SlaveDescription_t *getSlaveDescription();
+
     hopsan::HopsanEssentials *mpHopsanCore;
     hopsan::ComponentSystem *mpRootSystem;
     std::vector<std::string> mInputs, mOutputs;
+    std::vector<double*> mInputNodePtrs, mOutputNodePtrs;
+    std::vector<double*> mInputDataPtrs, mOutputDataPtrs;
     std::string mHost = "127.0.0.1";
     int mPort = 8080;
-    void printWaitingMessages();
-    SlaveDescription_t *getSlaveDescription();
+    DcpManagerSlave *mManager;
+    OstreamLog *stdLog;
+    UdpDriver* udpDriver;
+    double mSimulationTime=0;
+
+    void configure();
+    void initialize();
+    void doStep(uint64_t steps);
+    void setTimeRes(const uint32_t numerator, const uint32_t denominator);
 };
 
 #endif // DCPSLAVE_H
