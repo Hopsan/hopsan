5d44475e8ebb5aa43d7f3c27a54dc01823aa3653
Robert Braun <robert.braun@liu.se>
2022-03-28 14:00:59 +0200

    Added fmi4c dependency

---
+++ dependencies/setupFmi4c.bat
@@ -0,0 +1,32 @@
+@ECHO OFF
+
+REM Bat script for building fmi4c dependency automatically
+
+setlocal
+set basedir=%~dp0
+set name=fmi4c
+set codedir=%basedir%\%name%-code
+set builddir=%basedir%\%name%-build
+set installdir=%basedir%\%name%
+
+REM Setup PATH
+call setHopsanBuildPaths.bat
+
+REM build
+if exist %builddir% (
+  echo Removing existing build directory %builddir%
+  rmdir /S /Q %builddir%
+)
+mkdir %builddir%
+cd %builddir%
+cmake -Wno-dev -G %HOPSAN_BUILD_CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=%installdir% %codedir%
+cmake --build . --parallel 8
+cmake --build . --target install
+
+cd %basedir%
+echo.
+echo setupFmi4c.bat done!"
+if "%HOPSAN_BUILD_SCRIPT_NOPAUSE%" == "" (
+  pause
+)
+endlocal
