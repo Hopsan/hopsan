c68ff4f7f02cb1e76f7c8cbe05f8a4ab5b41a016
Peter Nordin <jpn.peter@gmail.com>
2022-04-21 22:10:15 +0200

    Fix setupFmi4c scripts
    Require newer fmi4c code then when is in dependencies.xml
    will update later when new library version is stable

--- dependencies/setupFmi4c.sh
+++ dependencies/setupFmi4c.sh
@@ -21,16 +21,13 @@ mkdir -p $builddir
 cd $builddir
 
 # Generate makefiles
-cmake -Wno-dev -G %HOPSAN_BUILD_CMAKE_GENERATOR% -DCMAKE_INSTALL_PREFIX=${installdir} ${codedir}
+cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=${installdir} ${codedir}
 
 # Build and install
 cmake --build . --parallel 8
 cmake --build . --target install
 
 
-cp -R ${codedir}/include ${installdir}/
-cp -R ${codedir}/3rdparty/fmi ${installdir}/3rdparty/
-
 # Return to basedir
 cd $basedir
-echo "setupFMILibrary.sh done!"
+echo "setupFmi4c.sh done!"
