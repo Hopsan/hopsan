b65424e9009a2c5a2e7123e304141ee15f9d802f
Peter Nordin <jpn.peter@gmail.com>
2022-12-15 23:20:16 +0100

    Update FMI4C build scripts after new FMI4c was added

--- dependencies/setupFmi4c.bat
+++ dependencies/setupFmi4c.bat
@@ -19,7 +19,7 @@ if exist %builddir% (
 )
 mkdir %builddir%
 cd %builddir%
-cmake -Wno-dev -G %HOPSAN_BUILD_CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%installdir% -DBUILD_SHARED=OFF -DUSE_INCLUDED_ZLIB=ON %codedir%
+cmake -Wno-dev -G %HOPSAN_BUILD_CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%installdir% -DFMI4C_BUILD_SHARED=OFF -DFMI4C_USE_INCLUDED_ZLIB=ON %codedir%
 cmake --build . --config Release --parallel 8
 cmake --build . --config Release --target install
 
