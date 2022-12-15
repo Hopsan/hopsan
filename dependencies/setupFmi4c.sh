49df3b87666a308aa6bcdf078865b2ac8105ccf4
Peter Nordin <jpn.peter@gmail.com>
2022-12-15 23:20:16 +0100

    Update FMI4C build scripts after new FMI4c was added

--- dependencies/setupFmi4c.sh
+++ dependencies/setupFmi4c.sh
@@ -21,7 +21,7 @@ mkdir -p $builddir
 cd $builddir
 
 # Generate makefiles
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=${installdir} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED=OFF ${codedir}
+cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=${installdir} -DCMAKE_BUILD_TYPE=Release -DFMI4C_BUILD_SHARED=OFF ${codedir}
 
 # Build and install
 cmake --build . --parallel 8
