9ec768f902e00d89d46f3c3429bc83af2adfc53f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-18 05:42:01 +0000

    Adding tarfile member sanitization to extractall()

--- dependencies/download-dependencies.py
+++ dependencies/download-dependencies.py
@@ -113,7 +113,26 @@ def clear_and_unpack(archive_file, dep_name):
             zipf.extractall(codedir)
     else:
         with tarfile.open(archive_file, 'r:*') as tarf:
-            tarf.extractall(codedir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tarf, codedir)
 
     # Figure out the name of the unpacked root dir
     root_dir_name = str()
