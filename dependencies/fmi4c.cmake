af89c8608e04b384d5dba47fb0c404a252998153
Peter Nordin <jpn.peter@gmail.com>
2023-04-27 22:50:05 +0200

    Use correct zlibstatic name when looking for static zlib

--- dependencies/fmi4c.cmake
+++ dependencies/fmi4c.cmake
@@ -2,13 +2,13 @@ set(local_fmi4c_dir ${CMAKE_CURRENT_LIST_DIR}/fmi4c)
 
 if (EXISTS ${local_fmi4c_dir})
 
-  set(zlib_name zlib)
+  set(zlibstatic_name zlibstatic)
   if (NOT WIN32)
-    set(zlib_name z)
+    set(zlibstatic_name z)
   endif()
 
   set(static_fmi4c ${local_fmi4c_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}fmi4c${CMAKE_STATIC_LIBRARY_SUFFIX})
-  set(static_zlib ${local_fmi4c_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}${zlib_name}${CMAKE_STATIC_LIBRARY_SUFFIX})
+  set(static_zlib ${local_fmi4c_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}${zlibstatic_name}${CMAKE_STATIC_LIBRARY_SUFFIX})
 
   add_library(fmi4c STATIC IMPORTED)
 
@@ -18,7 +18,7 @@ if (EXISTS ${local_fmi4c_dir})
                               INTERFACE_COMPILE_DEFINITIONS "USEFMI4C;FMI4C_STATIC")
 
   if (EXISTS ${static_zlib})
-      message(STATUS "Found static zlib in Fmi4c")
+      message(STATUS "Found static zlib in ${local_fmi4c_dir}")
       add_library(fmi4c_zlib STATIC IMPORTED)
       set_target_properties(fmi4c_zlib PROPERTIES
                                        IMPORTED_LOCATION ${static_zlib})
