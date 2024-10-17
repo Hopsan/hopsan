91d492d7a23cce6d9f8bdacc30c31b9f72595103
Robert Braun <robert.braun@liu.se>
2024-10-17 14:56:34 +0200

    Remove unneeded patch

--- dependencies/setupDCPLib.bat
+++ dependencies/setupDCPLib.bat
@@ -12,8 +12,6 @@ set xercesdir=%basedir:\=/%xerces
 
 call setHopsanBuildPaths.bat
 
-patch dcplib-code/include/core/dcp/model/constant/DcpPduType.hpp dcplib-patch.txt
-
 mkdir %builddir%
 cd %builddir%
 cmake -Wno-dev -G %HOPSAN_BUILD_CMAKE_GENERATOR% -DLOGGING=ON -DASIO_ROOT="%basedir%\asio-code" -DXercesC_LIBRARY="%xercesdir%/bin/libxerces-c.dll" -DXercesC_INCLUDE_DIR="%xercesdir%/include" -DXercesC_VERSION="3.2.2" -DZIP_LIBRARY="%basedir%\libzip\bin\libzip.dll" -DZIP_INCLUDE_DIR="%basedir%\libzip\include" -DCMAKE_INSTALL_PREFIX="%installdir%" %codedir%
