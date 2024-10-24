ac57f421b312f195cf73d441aaa0d239b962dc6d
Robert Braun <robert.braun@liu.se>
2024-10-24 14:16:36 +0200

    Only download zlib on Windows, and add it to setupAll.bat

--- dependencies/setupAll.bat
+++ dependencies/setupAll.bat
@@ -9,5 +9,6 @@ start /wait cmd /c setupKatex.bat
 start /wait cmd /c setupTclap.bat
 start /wait cmd /c setupHDF5.bat
 start /wait cmd /c setupXerces.bat
+start /wait cmd /c setupZlib.bat
 start /wait cmd /c setupLibzip.bat
 start /wait cmd /c setupDCPLib.bat
