2aea14e2bf6951d08a6911d5af81009b223b3ada
Peter Nordin <jpn.peter@gmail.com>
2024-12-04 22:33:32 +0100

    Install dependencies zlib, libzip and xerces

--- dependencies/dcplib.cmake
+++ dependencies/dcplib.cmake
@@ -1,4 +1,5 @@
 set(local_xerces_dir ${CMAKE_CURRENT_LIST_DIR}/xerces)
+set(local_zlib_dir ${CMAKE_CURRENT_LIST_DIR}/zlib)
 set(local_libzip_dir ${CMAKE_CURRENT_LIST_DIR}/libzip)
 set(local_asio_dir ${CMAKE_CURRENT_LIST_DIR}/asio-code)
 set(local_dcplib_dir ${CMAKE_CURRENT_LIST_DIR}/dcplib)
@@ -18,3 +19,47 @@ set_target_properties(ASIO::ASIO PROPERTIES
 target_link_libraries(ASIO::ASIO INTERFACE Threads::Threads)
 
 find_package(DCPLib CONFIG PATHS ${local_dcplib_dir} NO_DEFAULT_PATH)
+
+
+# Add zlib to installation if local variant found
+if (EXISTS ${local_zlib_dir})
+  # When building with mingw, zlib uses the lib prefix on windows
+  if (MINGW)
+    file(GLOB lib_files ${local_zlib_dir}/bin/libzlib${CMAKE_SHARED_LIBRARY_SUFFIX})
+  else()
+    file(GLOB lib_files ${local_zlib_dir}/bin/zlib${CMAKE_SHARED_LIBRARY_SUFFIX})
+  endif()
+  install(FILES ${lib_files} DESTINATION bin)
+endif()
+
+# Add libzip to installation if local variant found
+if (EXISTS ${local_libzip_dir})
+  # When building with mingw, libzip uses the lib prefix on windows
+  if (WIN32)
+    if (MINGW)
+      file(GLOB lib_files ${local_libzip_dir}/bin/libzip${CMAKE_SHARED_LIBRARY_SUFFIX})
+    else()
+      file(GLOB lib_files ${local_libzip_dir}/bin/zip*${CMAKE_SHARED_LIBRARY_SUFFIX})
+    endif()
+    install(FILES ${lib_files} DESTINATION bin)
+  else()
+    file(GLOB lib_files ${local_libzip_dir}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}zip${CMAKE_SHARED_LIBRARY_SUFFIX}*)
+    install(FILES ${lib_files} DESTINATION lib)
+  endif()
+endif()
+
+# Add xerces to installation if local variant found
+if (EXISTS ${local_xerces_dir})
+  # When building with mingw, xerces uses the lib prefix on windows
+  if (WIN32)
+    if (MINGW)
+      file(GLOB lib_files ${local_xerces_dir}/bin/libxerces-c${CMAKE_SHARED_LIBRARY_SUFFIX})
+    else()
+      file(GLOB lib_files ${local_xerces_dir}/bin/xerces-c*${CMAKE_SHARED_LIBRARY_SUFFIX})
+    endif()
+    install(FILES ${lib_files} DESTINATION bin)
+  else()
+    file(GLOB lib_files ${local_xerces_dir}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}xerces-c*${CMAKE_SHARED_LIBRARY_SUFFIX}*)
+    install(FILES ${lib_files} DESTINATION lib)
+  endif()
+endif()
