784aff1c17ab43193159b831855c96bf55b9faab
Robert Braun <robert.braun@liu.se>
2025-08-13 14:11:31 +0200

    Use included ZLIB for fmi4c

--- dependencies/setupFmi4c.bat
+++ dependencies/setupFmi4c.bat
@@ -20,7 +20,7 @@ if exist %builddir_d% (
 )
 mkdir %builddir_d%
 pushd %builddir_d%
-cmake -Wno-dev -G %HOPSAN_BUILD_CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=Debug -DCMAKE_DEBUG_POSTFIX="d" -DCMAKE_INSTALL_PREFIX=%installdir% -DFMI4C_BUILD_SHARED=OFF -DFMI4C_USE_INCLUDED_ZLIB=ON %codedir%
+cmake -Wno-dev -G %HOPSAN_BUILD_CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=Debug -DCMAKE_DEBUG_POSTFIX="d" -DCMAKE_INSTALL_PREFIX=%installdir% -DFMI4C_BUILD_SHARED=OFF -DFMI4C_USE_SYSTEM_ZIP=OFF -DFMI4C_USE_INCLUDED_ZLIB=ON %codedir%
 cmake --build . --config Debug --parallel 8
 cmake --build . --config Debug --target install
 popd
@@ -32,7 +32,7 @@ if exist %builddir_r% (
 )
 mkdir %builddir_r%
 pushd %builddir_r%
-cmake -Wno-dev -G %HOPSAN_BUILD_CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=Release -DCMAKE_DEBUG_POSTFIX="d" -DCMAKE_INSTALL_PREFIX=%installdir% -DFMI4C_BUILD_SHARED=OFF -DFMI4C_USE_INCLUDED_ZLIB=ON %codedir%
+cmake -Wno-dev -G %HOPSAN_BUILD_CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=Release -DCMAKE_DEBUG_POSTFIX="d" -DCMAKE_INSTALL_PREFIX=%installdir% -DFMI4C_BUILD_SHARED=OFF -DFMI4C_USE_SYSTEM_ZIP=OFF -DFMI4C_USE_INCLUDED_ZLIB=ON %codedir%
 cmake --build . --config Release --parallel 8
 cmake --build . --config Release --target install
 popd
