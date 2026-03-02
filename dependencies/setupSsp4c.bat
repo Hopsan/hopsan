b34850ba65c0770d37a94d4e753d89746c2321cb
Robert Braun <robert.braun@liu.se>
2026-03-02 14:22:53 +0100

    Load FMUs and connetors from SSD

---
+++ dependencies/setupSsp4c.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+
+REM Bat script for building ssp4c dependency automatically
+
+setlocal
+set basedir=%~dp0
+set name=ssp4c
+set codedir=%basedir%\%name%-code
+set builddir=%basedir%\%name%-build
+set installdir=%basedir%\%name%
+
+REM Setup PATH
+call setHopsanBuildPaths.bat
+
+REM build and install
+if exist %builddir% (
+  echo Removing existing build directory %builddir%
+  rmdir /S /Q %builddir%
+)
+mkdir %builddir%
+pushd %builddir%
+
+cmake -Wno-dev -G %HOPSAN_BUILD_CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%installdir% -DSSP4C_BUILD_SHARED=OFF %codedir%
+cmake --build . --config Debug --parallel 8
+cmake --build . --config Debug --target install
+popd
+
+echo.
+echo setupSsp4c.bat done!"
+if "%HOPSAN_BUILD_SCRIPT_NOPAUSE%" == "" (
+  pause
+)
+endlocal
