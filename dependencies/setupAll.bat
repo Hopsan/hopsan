ee528f6c50f5afdf21a34ea95d9671678902e421
Peter Nordin <jpn.peter@gmail.com>
2026-03-14 14:14:34 +0100

    Reorder setupAll scripts

--- dependencies/setupAll.bat
+++ dependencies/setupAll.bat
@@ -1,14 +1,14 @@
 set HOPSAN_BUILD_SCRIPT_NOPAUSE=true
 start /wait cmd /c setupDiscount.bat
 start /wait cmd /c setupFmi4c.bat
-start /wait cmd /c setupFMILibrary.bat
-start /wait cmd /c setupQwt.bat
-start /wait cmd /c setupMsgpack.bat
-start /wait cmd /c setupZeroMQ.bat
-start /wait cmd /c setupKatex.bat
-start /wait cmd /c setupTclap.bat
 start /wait cmd /c setupHDF5.bat
-start /wait cmd /c setupXerces.bat
+start /wait cmd /c setupKatex.bat
+start /wait cmd /c setupMsgpack.bat
+start /wait cmd /c setupQwt.bat
+start /wait cmd /c setupTclap.bat
+start /wait cmd /c setupZeroMQ.bat
 start /wait cmd /c setupZlib.bat
 start /wait cmd /c setupLibzip.bat
+start /wait cmd /c setupXerces.bat
+REM DCPLib must come after xerces and libzip (zlib before libzip)
 start /wait cmd /c setupDCPLib.bat
