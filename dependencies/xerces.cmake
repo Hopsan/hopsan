6f3bd7a7a2ac882aa86bf84ce5c807eb0e55ccba
Peter Nordin <jpn.peter@gmail.com>
2026-03-20 21:31:01 +0100

    Update xerces build and import on Windows

--- dependencies/xerces.cmake
+++ dependencies/xerces.cmake
@@ -1,38 +1,14 @@
 if (NOT TARGET xercesc)
     set(local_xerces_dir ${CMAKE_CURRENT_LIST_DIR}/xerces)
-    set(CMAKE_FIND_LIBRARY_PREFIXES lib)
-    find_library(xercesc NAMES xerces-c ${CMAKE_STATIC_LIBRARY_PREFIX}xerces-c_3D${CMAKE_STATIC_LIBRARY_SUFFIX} PATHS ${local_xerces_dir}/lib)
+    find_package(XercesC CONFIG PATHS ${local_xerces_dir} NO_DEFAULT_PATH)
 
-    #set(CMAKE_FIND_DEBUG_MODE TRUE)
-    #find_package(xercesc CONFIG PATHS ${local_xerces_dir} NO_DEFAULT_PATH REQUIRED) # Search for local version
-    #set(CMAKE_FIND_DEBUG_MODE FALSE)
-
-    if (xercesc)
+    if (XercesC_FOUND)
         message(STATUS "Found local xerces")
-        #add_library(xercesc INTERFACE)
-
-        add_library(xercesc STATIC IMPORTED)
-
-        if (MINGW)
-            set_target_properties(xercesc PROPERTIES
-                                    IMPORTED_LOCATION ${local_xerces_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}xerces-c${CMAKE_SHARED_LIBRARY_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}
-                                    INTERFACE_INCLUDE_DIRECTORIES ${local_xerces_dir}/include)
-        elseif(MSVC)
-            set_target_properties(xercesc PROPERTIES
-                                    IMPORTED_LOCATION ${local_xerces_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}xerces-c_3D${CMAKE_STATIC_LIBRARY_SUFFIX}
-                                    INTERFACE_INCLUDE_DIRECTORIES ${local_xerces_dir}/include)
-        else()
-            set_target_properties(xercesc PROPERTIES
-                                    IMPORTED_LOCATION ${local_xerces_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}xerces-c${CMAKE_SHARED_LIBRARY_SUFFIX}
-                                    INTERFACE_INCLUDE_DIRECTORIES ${local_xerces_dir}/include)
-        endif()
-
     else()
         message(STATUS "Looking for xerces in system")
         find_package(xercesc CONFIG) # Search in system
         if (xercesc_FOUND)
-            message(STATUS "  Found xerces")
-            add_library(xercesc INTERFACE)
+            message(STATUS "Found xerces in system")
         else()
             message(WARNING "Could not find xerces")
         endif()
