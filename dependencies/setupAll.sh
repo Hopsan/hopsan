918331bcdfd031204e819ed2d29b792990ca549e
Peter Nordin <jpn.peter@gmail.com>
2026-03-22 12:15:19 +0100

    Adjust buildorder

--- dependencies/setupAll.sh
+++ dependencies/setupAll.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+
+# First base libraries xerces depends on libzip
 ./setupAsio.sh
+./setupLibzip.sh
+./setupXerces.sh
+
+# DCPLib depends on xerces and libzip
+./setupDCPLib.sh
 ./setupDiscount.sh
 ./setupFmi4c.sh
 ./setupHDF5.sh
 ./setupKatex.sh
-./setupLibzip.sh
 ./setupMsgpack.sh
 ./setupQwt.sh
 ./setupTclap.sh
-./setupXerces.sh
 ./setupZeroMQ.sh
-# DCPLib must come after xerces and libzip
-./setupDCPLib.sh
 ./setupSsp4c.sh
