e2fda0dd5a33a7cd55381dc585b806a938ec2f49
Peter Nordin <jpn.peter@gmail.com>
2026-03-24 20:16:13 +0100

    Remove --fresh for older cmake

--- dependencies/setupXerces.bat
+++ dependencies/setupXerces.bat
@@ -19,14 +19,14 @@ if "%HOPSAN_BUILD_COMPILER%" == "msvc" (
   cmake -Wno-dev -G %HOPSAN_BUILD_CMAKE_GENERATOR% ^
         -DCMAKE_BUILD_TYPE=Debug ^
         -DCMAKE_INSTALL_PREFIX="%installdir%" ^
-        %codedir% --fresh
+        %codedir%
   cmake --build . --config Debug --parallel 8
   cmake --build . --config Debug --target install
 )
 cmake -Wno-dev -G %HOPSAN_BUILD_CMAKE_GENERATOR% ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_PREFIX="%installdir%" ^
-      %codedir% --fresh
+      %codedir%
 cmake --build . --config Release --parallel 8
 cmake --build . --config Release --target install
 
