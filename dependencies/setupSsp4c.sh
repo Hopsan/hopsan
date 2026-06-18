b786047453a61acdf04d6dff8e0afe2a62049c1e
Peter Nordin <jpn.peter@gmail.com>
2026-06-19 00:28:11 +0200

    Force lib dir name for libs in ssp4c

--- dependencies/setupSsp4c.sh
+++ dependencies/setupSsp4c.sh
@@ -21,7 +21,12 @@ mkdir -p $builddir
 cd $builddir
 
 # Generate makefiles
-cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -Wno-dev -DCMAKE_INSTALL_PREFIX=${installdir} -DCMAKE_BUILD_TYPE=Release -DSSP4C_BUILD_SHARED=OFF ${codedir}
+cmake -Wno-dev \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DSSP4C_BUILD_SHARED=OFF \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCMAKE_INSTALL_PREFIX=${installdir} \
+      ${codedir}
 
 # Build and install
 cmake --build . --parallel 8
