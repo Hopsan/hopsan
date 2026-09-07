91f7718dc591b63952168034763c845dbc9d6dd4
Robert Braun <robert.braun@liu.se>
2026-09-07 14:25:11 +0200

    Update version numbers for 2.24.3

--- makeDebRelease.sh
+++ makeDebRelease.sh
@@ -11,7 +11,7 @@ set -u
 hopsancode_root=$(pwd)
 pbuilderWorkDir=/var/tmp/deb_hopsan/pbuilder
 name=hopsan
-devversion=2.24.2
+devversion=2.24.3
 
 # Pbuilder dists and archs
 debianDistArchArray=( forky:amd64:forky
